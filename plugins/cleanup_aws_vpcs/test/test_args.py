8e8d98ca607e000618ed797300052ea33f165258
Lukas Lösche <lukas@mesosphere.io>
2020-06-23 04:02:48 +0200

    Add config support to cleanup AWS VPCs plugin

--- plugins/cleanup_aws_vpcs/test/test_args.py
+++ plugins/cleanup_aws_vpcs/test/test_args.py
@@ -7,3 +7,4 @@ def test_args():
     CleanupAWSVPCsPlugin.add_args(arg_parser)
     arg_parser.parse_args()
     assert ArgumentParser.args.cleanup_aws_vpcs is False
+    assert ArgumentParser.args.cleanup_aws_vpcs_config is None
