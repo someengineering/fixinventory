82e71a36ba2ed4d4896d15b21aa6bf161ddd52e2
Lukas Lösche <lukas@mesosphere.io>
2020-09-17 10:54:46 +0200

    Add extra args to test

--- plugins/gcp/test/test_args.py
+++ plugins/gcp/test/test_args.py
@@ -6,4 +6,11 @@ def test_args():
     arg_parser = get_arg_parser()
     GCPCollectorPlugin.add_args(arg_parser)
     arg_parser.parse_args()
-    assert ArgumentParser.args.gcp_region is None
+    assert ArgumentParser.args.gcp_zone is None
+    assert len(ArgumentParser.args.gcp_service_account) == 0
+    assert len(ArgumentParser.args.gcp_project) == 0
+    assert len(ArgumentParser.args.gcp_collect) == 0
+    assert len(ArgumentParser.args.gcp_no_collect) == 0
+    assert ArgumentParser.args.gcp_project_pool_size == 5
+    assert ArgumentParser.args.gcp_zone_pool_size == 20
+    assert ArgumentParser.args.gcp_fork is False
