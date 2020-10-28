273ab15d5e53eba0f46aa6d82a5b5475f69f826d
Lukas Lösche <lukas@mesosphere.io>
2020-10-28 15:53:46 +0100

    Update Manifest

--- cloudkeeper/setup.py
+++ cloudkeeper/setup.py
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     long_description=read("README.md"),
     entry_points={"console_scripts": ["cloudkeeper = cloudkeeper.__main__:main"]},
-    package_data={'cloudkeeper': ['static/favicon.ico']},
+    include_package_data=True,
     zip_safe=False,
     install_requires=[
         "networkx",
