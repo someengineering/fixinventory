b452c065738bb4d6107d99635e4e047138df8f83
Jan Ulferts <julferts@d2iq.com>
2021-05-28 12:28:36 +0200

    add dependency

--- plugins/vsphere/setup.py
+++ plugins/vsphere/setup.py
@@ -20,7 +20,7 @@ setup(
     },
     include_package_data=True,
     zip_safe=False,
-    install_requires=["cloudkeeper"],
+    install_requires=["cloudkeeper", "pyvmomi"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     classifiers=[
