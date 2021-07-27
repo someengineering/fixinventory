f0b6c6eb29b817086a3b212f8e9f106115533f60
Lukas Lösche <lukas@some.engineering>
2021-07-27 23:00:23 +0200

    Add alarm cleanup plugin

---
+++ plugins/cleanup_aws_alarms/test/test_args.py
@@ -0,0 +1,10 @@
+from cloudkeeper.args import get_arg_parser, ArgumentParser
+from cloudkeeper_plugin_cleanup_aws_alarms import CleanupAWSAlarmsPlugin
+
+
+def test_args():
+    arg_parser = get_arg_parser()
+    CleanupAWSAlarmsPlugin.add_args(arg_parser)
+    arg_parser.parse_args()
+    assert ArgumentParser.args.cleanup_aws_alarms is False
+    assert ArgumentParser.args.cleanup_aws_alarms_config is None
