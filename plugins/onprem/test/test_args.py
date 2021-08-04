166c16430f5c0d9a2d0ead35e152f3d831fe9ad4
Lukas Lösche <lukas@some.engineering>
2021-08-04 14:25:03 +0200

    Update args test

--- plugins/onprem/test/test_args.py
+++ plugins/onprem/test/test_args.py
@@ -6,4 +6,8 @@ def test_args():
     arg_parser = get_arg_parser()
     OnpremCollectorPlugin.add_args(arg_parser)
     arg_parser.parse_args()
-    assert ArgumentParser.args.onprem_region is None
+    assert ArgumentParser.args.onprem_location is None
+    assert ArgumentParser.args.onprem_subnet is None
+    assert ArgumentParser.args.onprem_user == "root"
+    assert ArgumentParser.args.onprem_ssh_key == "~/.ssh/id_rsa"
+    assert len(ArgumentParser.args.onprem_server) == 0
