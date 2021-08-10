b7e8d1fff13f076b25a1eb6120babb97be568823
Lukas Lösche <lukas@some.engineering>
2021-08-10 12:53:09 +0200

    Add cli command for edge statistics (#74)

---
+++ plugins/cli_edgestats/cloudkeeper_plugin_cli_edgestats/__init__.py
@@ -0,0 +1,41 @@
+import cloudkeeper.logging
+from typing import Iterable
+from cloudkeeper.baseresources import BaseResource
+from cloudkeeper.baseplugin import BaseCliPlugin
+from cloudkeeper.args import ArgumentParser
+from dataclasses import dataclass
+
+log = cloudkeeper.logging.getLogger("cloudkeeper." + __name__)
+
+
+@dataclass
+class Edge:
+    src: BaseResource
+    dst: BaseResource
+
+    @property
+    def srcdstrt(self) -> str:
+        return f"{self.src.resource_type}->{self.dst.resource_type}"
+
+
+class CliEdgestatsPlugin(BaseCliPlugin):
+    def cmd_edges(self, items: Iterable, args: str) -> Iterable:
+        """Usage: edges
+
+        Get all graph edges.
+        Example: edges | count srcdstrt
+                 edges | count src.resource_type
+        """
+        with self.graph.lock.read_access:
+            for edge in self.graph.edges:
+                if len(edge) == 2:
+                    src = edge[0]
+                    dst = edge[1]
+                    if isinstance(src, BaseResource) and isinstance(dst, BaseResource):
+                        e = Edge(src=src, dst=dst)
+                        yield e
+
+    @staticmethod
+    def add_args(arg_parser: ArgumentParser) -> None:
+        """Adds Plugin specific arguments to the global arg parser"""
+        pass
