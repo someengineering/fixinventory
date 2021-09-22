7e14823f314a0600d89ceb240341fa5cd190bc60
Lukas Lösche <lukas@some.engineering>
2021-09-22 14:06:43 +0200

    [cksh] Request text/plain instead of application/yaml (#187)
    * Request text/plain instead of application/yaml
    
    * Add terminal size to headers
    
    * Rename Lines -> Rows
    
    * Add support for reading inputs from stdin
    
    * Add debug logging

--- cksh/cksh/__main__.py
+++ cksh/cksh/__main__.py
@@ -1,46 +1,55 @@
 import sys
+import shutil
 import pathlib
 import requests
 from threading import Event
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from cklib.args import get_arg_parser, ArgumentParser
-from cklib.logging import log
+from cklib.logging import log, add_args as logging_add_args
+from typing import Dict
 
 
 def main() -> None:
+    shutdown_event = Event()
     arg_parser = get_arg_parser()
     add_args(arg_parser)
+    logging_add_args(arg_parser)
     arg_parser.parse_args()
-
-    session = completer = None
-    history_file = str(pathlib.Path.home() / ".cksh_history")
-    history = FileHistory(history_file)
-    session = PromptSession(history=history)
+    headers = {"Content-Type": "text/plain"}
     execute_endpoint = f"{ArgumentParser.args.keepercore_uri}/cli/execute"
-    shutdown_event = Event()
-    headers = {"Content-type": "application/yaml"}
+
+    if ArgumentParser.args.stdin:
+        log.debug("Reading commands from STDIN")
+        try:
+            for command in sys.stdin.readlines():
+                command = command.rstrip()
+                send_command(command, execute_endpoint, headers, tty=False)
+        except KeyboardInterrupt:
+            pass
+        except (RuntimeError, ValueError) as e:
+            log.error(e)
+        except Exception:
+            log.exception("Caught unhandled exception while processing CLI command")
+        finally:
+            shutdown_event.set()
+    else:
+        session = completer = None
+        history_file = str(pathlib.Path.home() / ".cksh_history")
+        history = FileHistory(history_file)
+        session = PromptSession(history=history)
+        log.debug("Starting interactive session")
 
     while not shutdown_event.is_set():
         try:
-            cli_input = session.prompt("> ", completer=completer)
-            if cli_input == "":
+            command = session.prompt("> ", completer=completer)
+            if command == "":
                 continue
-            if cli_input == "quit":
+            if command == "quit":
                 shutdown_event.set()
                 continue
 
-            r = requests.post(
-                execute_endpoint, data=cli_input, headers=headers, stream=True
-            )
-            if r.status_code != 200:
-                print(r.text, file=sys.stderr)
-                continue
-
-            for line in r.iter_lines():
-                if not line:
-                    continue
-                print(line.decode("utf-8"))
+            send_command(command, execute_endpoint, headers)
 
         except KeyboardInterrupt:
             pass
@@ -52,6 +61,41 @@ def main() -> None:
             log.exception("Caught unhandled exception while processing CLI command")
 
     sys.exit(0)
+
+
+def send_command(
+    command: str, execute_endpoint: str, headers: Dict[str, str], tty: bool = True
+) -> None:
+    if tty:
+        update_headers_with_terminal_size(headers)
+
+    log.debug(f'Sending command "{command}" to {execute_endpoint}')
+
+    r = requests.post(
+        execute_endpoint,
+        data=command,
+        headers=headers,
+        stream=True,
+    )
+    if r.status_code != 200:
+        print(r.text, file=sys.stderr)
+        return
+
+    for line in r.iter_lines():
+        if not line:
+            continue
+        print(line.decode("utf-8"))
+
+
+def update_headers_with_terminal_size(headers: Dict[str, str]) -> None:
+    tty_columns, tty_rows = shutil.get_terminal_size(fallback=(80, 20))
+    log.debug(f"Setting columns {tty_columns}, rows {tty_rows}")
+    headers.update(
+        {
+            "Cloudkeeper-Cksh-Columns": str(tty_columns),
+            "Cloudkeeper-Cksh-Rows": str(tty_rows),
+        }
+    )
 
 
 def add_args(arg_parser: ArgumentParser) -> None:
@@ -66,6 +110,13 @@ def add_args(arg_parser: ArgumentParser) -> None:
         help="Keepercore Websocket URI",
         default="ws://localhost:8080",
         dest="keepercore_ws_uri",
+    )
+    arg_parser.add_argument(
+        "--stdin",
+        help="Read from STDIN instead of opening a shell",
+        dest="stdin",
+        action="store_true",
+        default=False,
     )
 
 
