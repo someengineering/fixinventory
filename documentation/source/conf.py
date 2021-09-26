96110fe86d9253963f2be2a4656c03016cf07755
Neil Scholten <neil@some.engineering>
2021-09-26 12:14:20 +0200

    Documentation! First of many. This commit contains a getting started tutorial focussed on AWS Usage

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -37,7 +37,6 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.coverage',
     'sphinx_rtd_theme',
-]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
