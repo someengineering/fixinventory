a6d322c50c05666347bf5f0a6e6128ac282369be
Matthias Veit <matthias_veit@yahoo.de>
2021-09-27 11:30:40 +0200

    [ckcore] Allow escaping non conform json keys

--- ckcore/core/parse_util.py
+++ ckcore/core/parse_util.py
@@ -44,13 +44,26 @@ colon_dp = string(":")
 comma_dp = string(",")
 dot_dot_dp = string("..")
 equals_dp = string("=")
+backtick_dp = string("`")
 integer_dp = regex(r"[+-]?[0-9]+").map(int)
 float_dp = regex(r"[+-]?[0-9]+\.[0-9]+").map(float)
-variable_dp = regex("[A-Za-z][A-Za-z0-9_.*\\[\\]]*")
 literal_dp = regex("[A-Za-z0-9][A-Za-z0-9_\\-]*")
 
-allowed_characters = re.compile("[A-Za-z0-9_\\-:]")
-end_of_unquoted_str = re.compile("[\\[\\])(}{\\s]")
+# variables:
+# - simple props: foo, bla, bar
+# - nested props: foo.bla.bar
+# - array props: foo[*].bla[0].bar[1]
+# - props with non json conform keys: foo.bla.`:-)`.bar
+variable_dp_backtick_allowed = regex(r"[^`]+")
+variable_dp_backtick = backtick_dp + variable_dp_backtick_allowed + backtick_dp
+variable_dp_part_string = regex("[A-Za-z][A-Za-z0-9_]*")
+variable_dp_part_combine = regex("[0-9.*\\[\\]]*")
+variable_dp_part = variable_dp_part_string | variable_dp_backtick
+variable_dp = (variable_dp_part + variable_dp_part_combine).at_least(1).map("".join)
+
+
+unquoted_allowed_characters = re.compile("[A-Za-z0-9_\\-:]")
+unquoted_end_of_unquoted_str = re.compile("[\\[\\])(}{\\s]")
 
 
 @make_direct_parser
@@ -62,9 +75,9 @@ def unquoted_string_parser(stream: str, index: int) -> parsy.Result:
     found_alpha = False
     valid_string_chars = True
     # look ahead to next delimiter
-    while index < len(stream) and valid_string_chars and not end_of_unquoted_str.match(stream[index]):
+    while index < len(stream) and valid_string_chars and not unquoted_end_of_unquoted_str.match(stream[index]):
         found_alpha = str.isalpha(stream[index]) if not found_alpha else found_alpha
-        valid_string_chars = allowed_characters.match(stream[index]) is not None
+        valid_string_chars = unquoted_allowed_characters.match(stream[index]) is not None
         index += 1
 
     if index <= len(stream) and valid_string_chars and found_alpha:
