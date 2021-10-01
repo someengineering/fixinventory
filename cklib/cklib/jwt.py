2fcacd34ea91ae2e946c1eae41b75bca5a9b1c2e
Lukas Lösche <lukas@some.engineering>
2021-10-02 01:25:18 +0200

    Support to add JWT to and decode from http headers

--- cklib/cklib/jwt.py
+++ cklib/cklib/jwt.py
@@ -3,6 +3,7 @@ import jwt
 import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from typing import Optional
 
 
 def key_from_psk(psk: str, salt: bytes = None) -> tuple[bytes, bytes]:
@@ -29,3 +30,37 @@ def decode_jwt(encoded_jwt: str, psk: str) -> dict:
     salt = base64.standard_b64decode(salt_encoded)
     key, _ = key_from_psk(psk, salt)
     return jwt.decode(encoded_jwt, key, algorithms=["HS256"])
+
+
+def encode_jwt_to_headers(
+    http_headers: dict[str, str],
+    payload: dict[str, str],
+    psk: str,
+    scheme: str = "Bearer",
+) -> dict[str, str]:
+    http_headers.update({"Authorization": f"{scheme} {encode_jwt(payload, psk)}"})
+    return http_headers
+
+
+def decode_jwt_from_headers(
+    http_headers: dict[str, str], psk: str, scheme: str = "Bearer"
+) -> Optional[dict[str, str]]:
+    authorization_header = {
+        str(k).capitalize(): v for k, v in http_headers.items()
+    }.get("Authorization")
+    if authorization_header is None:
+        return None
+    return decode_jwt_from_header_value(authorization_header, psk, scheme)
+
+
+def decode_jwt_from_header_value(
+    authorization_header: str, psk: str, scheme: str = "Bearer"
+) -> Optional[dict[str, str]]:
+    if (
+        len(authorization_header) <= len(scheme) + 1
+        or str(authorization_header[0 : len(scheme)]).lower() != scheme.lower()
+        or authorization_header[len(scheme) : len(scheme) + 1] != " "
+    ):
+        return None
+    encoded_jwt = authorization_header[len(scheme) + 1 :]
+    return decode_jwt(encoded_jwt, psk)
--- cklib/test/test_jwt.py
+++ cklib/test/test_jwt.py
@@ -1,5 +1,11 @@
 import pytest
-from cklib.jwt import encode_jwt, decode_jwt
+from cklib.jwt import (
+    decode_jwt_from_header_value,
+    decode_jwt_from_headers,
+    encode_jwt,
+    decode_jwt,
+    encode_jwt_to_headers,
+)
 from jwt import InvalidSignatureError
 
 
@@ -8,9 +14,13 @@ def test_jwt():
     payload = {"Hello": "World"}
     j1 = encode_jwt(payload, psk)
     j2 = encode_jwt(payload, psk)
+    h1 = encode_jwt_to_headers({}, payload, psk)
     assert j1 != j2
     assert decode_jwt(j1, psk) == payload
     assert decode_jwt(j2, psk) == payload
     with pytest.raises(InvalidSignatureError) as e:
         decode_jwt(j1, "wrongpsk")
     assert str(e.value) == "Signature verification failed"
+    assert decode_jwt_from_headers(h1, psk) == payload
+    assert decode_jwt_from_headers({}, psk) is None
+    assert decode_jwt_from_header_value("", psk) is None
