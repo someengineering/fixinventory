98cf3f9962ed3c0f14252d37acf5628ccbbac031
Lukas Lösche <lukas@some.engineering>
2021-10-02 02:16:53 +0200

    Support to add JWT to and decode from http headers (#285)
    * Support to add JWT to and decode from http headers

--- cklib/cklib/jwt.py
+++ cklib/cklib/jwt.py
@@ -3,9 +3,14 @@ import jwt
 import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from typing import Any, Optional
 
 
 def key_from_psk(psk: str, salt: bytes = None) -> tuple[bytes, bytes]:
+    """Derive a 256 bit key from a passphrase/pre-shared-key.
+    A salt can be optionally provided. If not one will be generated.
+    Returns both the key and the salt.
+    """
     if salt is None:
         salt = os.urandom(16)
     kdf = PBKDF2HMAC(
@@ -18,14 +23,77 @@ def key_from_psk(psk: str, salt: bytes = None) -> tuple[bytes, bytes]:
     return key, salt
 
 
-def encode_jwt(payload: dict[str, str], psk: str) -> str:
+def encode_jwt(
+    payload: dict[str, str], psk: str, headers: Optional[dict[str, str]] = None
+) -> str:
+    """Encodes a payload into a JWT and signs using a key derived from a pre-shared-key.
+    Stores the key's salt in the JWT headers.
+    """
+    if headers is None:
+        headers = {}
     key, salt = key_from_psk(psk)
     salt_encoded = base64.standard_b64encode(salt).decode("utf-8")
-    return jwt.encode(payload, key, algorithm="HS256", headers={"salt": salt_encoded})
+    headers.update({"salt": salt_encoded})
+    return jwt.encode(payload, key, algorithm="HS256", headers=headers)
 
 
-def decode_jwt(encoded_jwt: str, psk: str) -> dict:
+def decode_jwt(
+    encoded_jwt: str, psk: str, options: Optional[dict[str, Any]] = None
+) -> dict:
+    """Decode a JWT using a key derived from a pre-shared-key and a salt stored
+    in the JWT headers.
+    """
     salt_encoded = jwt.get_unverified_header(encoded_jwt).get("salt")
     salt = base64.standard_b64decode(salt_encoded)
     key, _ = key_from_psk(psk, salt)
-    return jwt.decode(encoded_jwt, key, algorithms=["HS256"])
+    return jwt.decode(encoded_jwt, key, algorithms=["HS256"], options=options)
+
+
+def encode_jwt_to_headers(
+    http_headers: dict[str, str],
+    payload: dict[str, str],
+    psk: str,
+    scheme: str = "Bearer",
+    headers: Optional[dict[str, str]] = None,
+) -> dict[str, str]:
+    """Takes a payload and psk turns them into a JWT and adds that to a http headers
+    dictionary.
+    """
+    http_headers.update(
+        {"Authorization": f"{scheme} {encode_jwt(payload, psk, headers)}"}
+    )
+    return http_headers
+
+
+def decode_jwt_from_headers(
+    http_headers: dict[str, str],
+    psk: str,
+    scheme: str = "Bearer",
+    options: Optional[dict[str, Any]] = None,
+) -> Optional[dict[str, str]]:
+    """Retrieves the Authorization header from a http headers dictionary and
+    passes it to `decode_jwt_from_header_value()` to return the decoded payload.
+    """
+    authorization_header = {
+        str(k).capitalize(): v for k, v in http_headers.items()
+    }.get("Authorization")
+    if authorization_header is None:
+        return None
+    return decode_jwt_from_header_value(authorization_header, psk, scheme, options)
+
+
+def decode_jwt_from_header_value(
+    authorization_header: str,
+    psk: str,
+    scheme: str = "Bearer",
+    options: Optional[dict[str, Any]] = None,
+) -> Optional[dict[str, str]]:
+    """Decodes a JWT payload from a http Authorization header value."""
+    if (
+        len(authorization_header) <= len(scheme) + 1
+        or str(authorization_header[0 : len(scheme)]).lower() != scheme.lower()
+        or authorization_header[len(scheme) : len(scheme) + 1] != " "
+    ):
+        return None
+    encoded_jwt = authorization_header[len(scheme) + 1 :]
+    return decode_jwt(encoded_jwt, psk, options)
--- cklib/test/test_jwt.py
+++ cklib/test/test_jwt.py
@@ -1,6 +1,13 @@
 import pytest
-from cklib.jwt import encode_jwt, decode_jwt
-from jwt import InvalidSignatureError
+import datetime
+from cklib.jwt import (
+    decode_jwt_from_header_value,
+    decode_jwt_from_headers,
+    encode_jwt,
+    decode_jwt,
+    encode_jwt_to_headers,
+)
+from jwt import InvalidSignatureError, ExpiredSignatureError, ImmatureSignatureError, MissingRequiredClaimError
 
 
 def test_jwt():
@@ -8,9 +15,35 @@ def test_jwt():
     payload = {"Hello": "World"}
     j1 = encode_jwt(payload, psk)
     j2 = encode_jwt(payload, psk)
+    past_date = datetime.datetime.now() - datetime.timedelta(30)
+    future_date = datetime.datetime.now() + datetime.timedelta(30)
+
+    expired_payload = {"Hello": "World", "exp": past_date}
+    future_payload = {"Hello": "World", "exp": future_date}
+    nbf_payload = {"Hello": "World", "nbf": future_date}
+    expired_jwt = encode_jwt(expired_payload, psk)
+    valid_jwt = encode_jwt(future_payload, psk)
+    not_yet_valid_jwt = encode_jwt(nbf_payload, psk)
+
+    h1 = encode_jwt_to_headers({}, payload, psk)
+
     assert j1 != j2
     assert decode_jwt(j1, psk) == payload
     assert decode_jwt(j2, psk) == payload
+    assert decode_jwt_from_headers(h1, psk) == payload
+    assert decode_jwt_from_headers({}, psk) is None
+    assert decode_jwt_from_header_value("", psk) is None
     with pytest.raises(InvalidSignatureError) as e:
         decode_jwt(j1, "wrongpsk")
     assert str(e.value) == "Signature verification failed"
+    assert decode_jwt(expired_jwt, psk, options={"verify_exp": False}).get("Hello") == "World"
+    with pytest.raises(ExpiredSignatureError) as e:
+        decode_jwt(expired_jwt, psk)
+    assert str(e.value) == "Signature has expired"
+    assert decode_jwt(valid_jwt, psk).get("Hello") == "World"
+    with pytest.raises(ImmatureSignatureError) as e:
+        decode_jwt(not_yet_valid_jwt, psk)
+    assert str(e.value).startswith("The token is not yet valid")
+    with pytest.raises(MissingRequiredClaimError) as e:
+        decode_jwt(j1, psk, options={"require": ["exp"]})
+    assert str(e.value) == 'Token is missing the "exp" claim'
