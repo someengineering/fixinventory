2ac2001e133c5fad87577c99786c869741b404cd
Lukas Lösche <lukas@some.engineering>
2021-10-02 14:05:20 +0200

    Refactor ckmetrics

--- ckmetrics/ckmetrics/__main__.py
+++ ckmetrics/ckmetrics/__main__.py
@@ -1,18 +1,17 @@
 import sys
-import requests
-import json
 import time
 import inspect
 import cklib.baseresources
 from cklib.logging import log, add_args as logging_add_args
 from functools import partial
 from cklib.event import CkEvents
-from cklib.jwt import encode_jwt_to_headers
-from prometheus_client import Summary, start_http_server, REGISTRY
+from ckmetrics.metrics import Metrics, GraphCollector
+from ckmetrics.query import query, get_labels_from_result, get_metrics_from_result, get_label_values_from_result
+from ckmetrics.web import WebServer, CloudkeeperMetricsWebApp
+from prometheus_client import Summary, REGISTRY
 from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily
 from threading import Event
 from cklib.args import ArgumentParser
-from typing import Dict, Iterator, Tuple
 from signal import signal, SIGTERM, SIGINT
 
 
@@ -29,16 +28,6 @@ def handler(sig, frame) -> None:
     shutdown_event.set()
 
 
-class Metrics:
-    def __init__(self) -> None:
-        self.live = {}
-        self.staging = {}
-
-    def swap(self) -> None:
-        self.live = self.staging
-        self.staging = {}
-
-
 def main() -> None:
     signal(SIGINT, handler)
     signal(SIGTERM, handler)
@@ -48,6 +37,7 @@ def main() -> None:
     )
     add_args(arg_parser)
     logging_add_args(arg_parser)
+    WebServer.add_args(arg_parser)
     arg_parser.parse_args()
 
     metrics = Metrics()
@@ -59,7 +49,6 @@ def main() -> None:
     graph_uri = f"{base_uri}/graph/{ckcore_graph}"
     query_uri = f"{graph_uri}/reported/query/aggregate"
 
-    start_http_server(ArgumentParser.args.web_port)
     message_processor = partial(ckcore_message_processor, metrics, query_uri)
     ke = CkEvents(
         identifier="ckmetrics",
@@ -73,13 +62,17 @@ def main() -> None:
         },
         message_processor=message_processor,
     )
+    webserver = WebServer(CloudkeeperMetricsWebApp())
+    webserver.daemon = True
+    webserver.start()
     ke.start()
     shutdown_event.wait()
+    webserver.shutdown()
     ke.shutdown()
     sys.exit(0)
 
 
-def ckcore_message_processor(metrics: Metrics, query_uri: str, message: Dict) -> None:
+def ckcore_message_processor(metrics: Metrics, query_uri: str, message: dict) -> None:
     if not isinstance(message, dict):
         log.error(f"Invalid message: {message}")
         return
@@ -108,28 +101,6 @@ def ckcore_message_processor(metrics: Metrics, query_uri: str, message: Dict) ->
             "data": data,
         }
         return reply_message
-
-
-def query(query_str: str, query_uri: str) -> Iterator:
-    headers = {"Accept": "application/x-ndjson"}
-    if ArgumentParser.args.psk:
-        encode_jwt_to_headers(headers, {}, ArgumentParser.args.psk)
-
-    r = requests.post(
-        query_uri,
-        data=query_str,
-        headers=headers,
-        stream=True,
-    )
-    if r.status_code != 200:
-        raise RuntimeError(f"Failed to query graph: {r.content}")
-
-    for line in r.iter_lines():
-        if not line:
-            continue
-
-        data = json.loads(line.decode("utf-8"))
-        yield data
 
 
 def find_metrics(mod):
@@ -196,35 +167,7 @@ def update_metrics(metrics: Metrics, query_uri: str) -> None:
     metrics.swap()
 
 
-def get_metrics_from_result(result: Dict):
-    result_metrics = dict(result)
-    del result_metrics["group"]
-    return result_metrics
-
-
-def get_labels_from_result(result: Dict):
-    labels = tuple(result.get("group", {}).keys())
-    return labels
-
-
-def get_label_values_from_result(result: Dict, labels: Tuple):
-    label_values = []
-    for label in labels:
-        label_value = result.get("group", {}).get(label)
-        if label_value is None:
-            label_value = ""
-        label_values.append(str(label_value))
-    return tuple(label_values)
-
-
 def add_args(arg_parser: ArgumentParser) -> None:
-    arg_parser.add_argument(
-        "--web-port",
-        help="TCP port to listen on (default: 9955)",
-        default=9955,
-        type=int,
-        dest="web_port",
-    )
     arg_parser.add_argument(
         "--ckcore-uri",
         help="ckcore URI (default: http://localhost:8900)",
@@ -256,20 +199,6 @@ def add_args(arg_parser: ArgumentParser) -> None:
         dest="timeout",
         type=int,
     )
-
-
-class GraphCollector:
-    """A Prometheus compatible Collector implementation"""
-
-    def __init__(self, metrics: Metrics) -> None:
-        self.metrics = metrics
-
-    def collect(self):
-        """collect() is being called whenever the /metrics endpoint is requested"""
-        log.debug("generating metrics")
-
-        for metric in self.metrics.live.values():
-            yield metric
 
 
 if __name__ == "__main__":
--- ckmetrics/test/test_args.py
+++ ckmetrics/test/test_args.py
@@ -1,5 +1,7 @@
+from typing_extensions import ParamSpecKwargs
 from cklib.args import ArgumentParser
 from ckmetrics.__main__ import add_args
+from ckmetrics.web import WebServer
 
 
 def test_args():
@@ -7,5 +9,13 @@ def test_args():
         description="Cloudkeeper Metrics Exporter", env_args_prefix="CKMETRICS_"
     )
     add_args(arg_parser)
+    WebServer.add_args(arg_parser)
     arg_parser.parse_args()
+    assert ArgumentParser.args.ckcore_uri == "http://localhost:8900"
+    assert ArgumentParser.args.ckcore_ws_uri == "ws://localhost:8900"
+    assert ArgumentParser.args.ckcore_graph == "ck"
+    assert ArgumentParser.args.psk is None
+    assert ArgumentParser.args.timeout == 300
     assert ArgumentParser.args.web_port == 9955
+    assert ArgumentParser.args.web_host == "::"
+    assert ArgumentParser.args.web_path == "/"
