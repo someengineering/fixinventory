6dd3a87a4c2e1cc4a1f99bb05aff232a396d60c1
Lukas Lösche <lukas@some.engineering>
2021-10-06 14:01:50 +0200

    Move webserver and metrics webapp to cklib

--- cklib/cklib/web/__init__.py
+++ cklib/cklib/web/__init__.py
@@ -1,36 +1,8 @@
 from prometheus_client.exposition import generate_latest, CONTENT_TYPE_LATEST
 from cklib.args import ArgumentParser
-import os
 import cherrypy
 import threading
 from cklib.logging import log
-
-
-class CloudkeeperMetricsWebApp:
-    def __init__(self) -> None:
-        self.mountpoint = ArgumentParser.args.web_path
-        local_path = os.path.abspath(os.path.dirname(__file__))
-        config = {
-            "tools.gzip.on": True,
-            "tools.staticdir.index": "index.html",
-            "tools.staticdir.on": True,
-            "tools.staticdir.dir": f"{local_path}/static",
-        }
-        self.config = {"/": config}
-        if self.mountpoint not in ("/", ""):
-            self.config[self.mountpoint] = config
-
-    @cherrypy.expose
-    @cherrypy.tools.allow(methods=["GET"])
-    def health(self):
-        cherrypy.response.headers["Content-Type"] = "text/plain"
-        return "ok\r\n"
-
-    @cherrypy.expose
-    @cherrypy.tools.allow(methods=["GET"])
-    def metrics(self):
-        cherrypy.response.headers["Content-Type"] = CONTENT_TYPE_LATEST
-        return generate_latest()
 
 
 class WebServer(threading.Thread):
@@ -102,12 +74,5 @@ class WebServer(threading.Thread):
             help="IP to bind to (default: ::)",
             default="::",
             dest="web_host",
-            type=str,
-        )
-        arg_parser.add_argument(
-            "--web-path",
-            help="Web root in browser (default: /)",
-            default="/",
-            dest="web_path",
             type=str,
         )
---
+++ cklib/cklib/web/metrics.py
@@ -0,0 +1,35 @@
+from prometheus_client.exposition import generate_latest, CONTENT_TYPE_LATEST
+from cklib.args import ArgumentParser
+import os
+import cherrypy
+
+
+class WebApp:
+    def __init__(self) -> None:
+        self.mountpoint = ArgumentParser.args.web_path
+        local_path = os.path.abspath(os.path.dirname(__file__))
+        config = {
+            "tools.gzip.on": True,
+            "tools.staticdir.index": "index.html",
+            "tools.staticdir.on": True,
+            "tools.staticdir.dir": f"{local_path}/static",
+        }
+        self.config = {"/": config}
+        if self.mountpoint not in ("/", ""):
+            self.config[self.mountpoint] = config
+
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=["GET"])
+    def metrics(self):
+        cherrypy.response.headers["Content-Type"] = CONTENT_TYPE_LATEST
+        return generate_latest()
+
+    @staticmethod
+    def add_args(arg_parser: ArgumentParser) -> None:
+        arg_parser.add_argument(
+            "--web-path",
+            help="Web root in browser (default: /)",
+            default="/",
+            dest="web_path",
+            type=str,
+        )
