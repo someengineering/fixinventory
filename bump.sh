fc36c17abaa61c8e11e9be53742850bbeb428b24
Lukas Lösche <lukas@some.engineering>
2021-10-06 11:18:42 +0200

    Add a simple bump script (#313)
    * Add simple bump script

---
+++ bump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <bump-from> <bump-to>"
+    echo "E.g. $0 '2\\.0\\.0a5' 2.0.0a6"
+    exit 1
+fi
+
+bump_from=$1
+bump_to=$2
+find cklib \
+        ckcore \
+        ckworker \
+        cksh \
+        ckmetrics \
+        plugins \
+        cloudkeeperV1 \
+    -name setup.py -o \
+    -name __init__.py -o \
+    -name requirements.txt \
+| xargs grep "$bump_from" \
+| cut -d : -f 1 \
+| xargs sed -i -e "s/$bump_from/$bump_to/g"
+git status
+git diff
