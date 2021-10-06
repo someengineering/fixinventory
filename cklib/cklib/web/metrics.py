8648c82437ca3b07b219a60288226a2a38a5f373
Lukas Lösche <lukas@some.engineering>
2021-10-06 16:06:11 +0200

    Add health endpoint back

--- cklib/cklib/web/metrics.py
+++ cklib/cklib/web/metrics.py
@@ -20,6 +20,12 @@ class WebApp:
 
     @cherrypy.expose
     @cherrypy.tools.allow(methods=["GET"])
+    def health(self):
+        cherrypy.response.headers["Content-Type"] = "text/plain"
+        return "ok\r\n"
+
+    @cherrypy.expose
+    @cherrypy.tools.allow(methods=["GET"])
     def metrics(self):
         cherrypy.response.headers["Content-Type"] = CONTENT_TYPE_LATEST
         return generate_latest()
