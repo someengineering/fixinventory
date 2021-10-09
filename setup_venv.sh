012131a30539ff81737839ce2795eb2b638892c4
Lukas Lösche <lukas@some.engineering>
2021-10-09 05:31:10 +0200

    Allow setup_venv to update an existing venv

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 if [ -d "venv/" ]; then
     echo -e "Virtual Python ENV already exists!\nRun:\n\trm -rf venv/\nif you want to recreate it"
-    exit 1
+else
+    python3.9 -m venv venv
 fi
-python3.9 -m venv venv
 source venv/bin/activate
 pip install -U pip
 pip install -r ckcore/requirements-dev.txt
