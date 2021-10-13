eb88570c22109fcc4b3ba5254407d30f276d2695
Raffaele Picca <picster@pixelgod.net>
2021-10-13 14:50:07 +0200

    Creates filter list by parsing the graph.data.json

--- ckui/src/ui/elements/scripts/element_popup_load_data.gd
+++ ckui/src/ui/elements/scripts/element_popup_load_data.gd
@@ -2,53 +2,7 @@ extends Popup
 
 signal ok
 
-const FILTERS = [
-	"graph_root",
-	"cloud",
-	"aws_account",
-	"aws_region",
-	"aws_account",
-	"aws_ec2_instance_type",
-	"aws_ec2_instance_quota",
-	"aws_ec2_instance",
-	"aws_ec2_keypair",
-	"aws_ec2_volume_type",
-	"aws_ec2_volume",
-	"aws_ec2_snapshot",
-	"aws_ec2_subnet",
-	"aws_ec2_elastic_ip",
-	"aws_vpc",
-	"aws_vpc_quota",
-	"aws_s3_bucket",
-	"aws_s3_bucket_quota",
-	"aws_elb",
-	"aws_alb",
-	"aws_alb_target_group",
-	"aws_elb_quota",
-	"aws_alb_quota",
-	"aws_ec2_internet_gateway",
-	"aws_ec2_nat_gateway",
-	"aws_ec2_internet_gateway_quota",
-	"aws_ec2_security_group",
-	"aws_ec2_route_table",
-	"aws_vpc_peering_connection",
-	"aws_vpc_endpoint",
-	"aws_ec2_network_acl",
-	"aws_ec2_network_interface",
-	"aws_rds_instance",
-	"aws_iam_group",
-	"aws_iam_role",
-	"aws_iam_policy",
-	"aws_iam_instance_profile",
-	"aws_iam_access_key",
-	"aws_iam_server_certificate",
-	"aws_iam_server_certificate_quota",
-	"aws_cloudformation_stack",
-	"aws_eks_cluster",
-	"aws_eks_nodegroup",
-	"aws_autoscaling_group",
-	"aws_cloudwatch_alarm"
-	]
+const filters = []
 
 const TEXT_NOFILE = "data/graph.dump.json NOT found!\nYou can continue with generated example data or place the file in the data/ folder inside the project."
 const TEXT_FILEFOUND = "data/graph.dump.json found ({0}MB)!"
@@ -63,14 +17,33 @@ onready var grid = $Margin/MarginContainer/Content/VBoxContainer/GridContainer
 
 func _ready():
 	_on_Timer_timeout()
-	for filter in FILTERS:
+	_e.connect("load_nodes", self, "popup_show")
+
+
+func create_filters():
+	var file = File.new()
+	var filters := []
+	
+	if file.file_exists(_g.GRAPH_DUMP_JSON_PATH):
+		file.open(_g.GRAPH_DUMP_JSON_PATH, file.READ)
+		while !file.eof_reached():
+			var line = file.get_line()
+			if line == "":
+				continue
+			
+			var next_line = parse_json(line)
+			if "reported" in next_line:
+				if !next_line.reported.kind in filters:
+					filters.append(next_line.reported.kind)
+		file.close()
+	
+	for filter in filters:
 		var new_checkbox = $Margin/MarginContainer/Content/VBoxContainer/CheckBox.duplicate()
 		new_checkbox.text = filter
 		new_checkbox.name = filter
 		new_checkbox.show()
 		grid.add_child(new_checkbox)
-	_e.connect("load_nodes", self, "popup_show")
-	
+
 
 func check_for_graph_dump() -> bool:
 	var file = File.new()
@@ -87,6 +60,7 @@ func set_file_found(value:bool):
 	if value != file_found:
 		file_found = value
 		if file_found:
+			create_filters()
 			output.text = TEXT_FILEFOUND.format([ str(stepify(file_size/1000000, 0.01)) ])
 			$Margin/MarginContainer/Content/VBoxContainer/FilterButtons.show()
 			$Margin/MarginContainer/Content/VBoxContainer/GridContainer.show()
