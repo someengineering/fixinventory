b3d71850ec3faaee5e390b3407f17580f86be5cf
Lukas Lösche <lukas@some.engineering>
2021-10-16 13:09:58 +0200

    Wait for services to startup

--- docker/service.in/ckcore.sh
+++ docker/service.in/ckcore.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+. /usr/local/etc/cloudkeeper/defaults
+if [ "$START_GRAPHDB" = true ]; then
+    sv start graphdb || exit 1
+fi
 source /usr/local/cloudkeeper-venv-pypy3/bin/activate
 exec /sbin/setuser cloudkeeper ckcore @CLOUDKEEPER_ARGSDISPATCHER@
