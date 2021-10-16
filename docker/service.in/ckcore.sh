b32a834b4bfa24ed8b9cf2cf3d0be13aa38a4467
Lukas Lösche <lukas@some.engineering>
2021-10-16 13:12:22 +0200

    Wait for DB bootstrapping

--- docker/service.in/ckcore.sh
+++ docker/service.in/ckcore.sh
@@ -2,6 +2,7 @@
 . /usr/local/etc/cloudkeeper/defaults
 if [ "$START_GRAPHDB" = true ]; then
     sv start graphdb || exit 1
+    sleep 7
 fi
 source /usr/local/cloudkeeper-venv-pypy3/bin/activate
 exec /sbin/setuser cloudkeeper ckcore @CLOUDKEEPER_ARGSDISPATCHER@
