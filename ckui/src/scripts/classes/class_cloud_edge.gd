46cac1d94fce266e8f449be1b3dbb7ea3339a6c8
Raffaele Picca <picster@pixelgod.net>
2021-10-22 10:32:53 +0200

    Changed edge appearance

--- ckui/src/scripts/classes/class_cloud_edge.gd
+++ ckui/src/scripts/classes/class_cloud_edge.gd
@@ -4,7 +4,8 @@ class_name CloudEdge
 var from : CloudNode = null
 var to : CloudNode = null
 var line : Line2D = null
-var color := Color(0.7,0.9,1,0.1)
+var color := Color(0.7,0.9,1,0.5)
+var colors_temp := [Color(0.7,0.9,1,0.5), Color(1.0,0.5,0.1,0.5)]
 
 func clone(original : CloudEdge):
 	color = original.color
