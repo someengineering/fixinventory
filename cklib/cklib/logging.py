8057312d2839e456f79d9d68a45b15f381d1106e
Lukas Lösche <lukas@some.engineering>
2021-11-04 18:00:11 +0100

    Add --quiet option

--- cklib/cklib/logging.py
+++ cklib/cklib/logging.py
@@ -15,20 +15,12 @@ from cklib.args import ArgumentParser
 getLogger().setLevel(ERROR)
 getLogger("cloudkeeper").setLevel(INFO)
 
-argv = sys.argv[1:]
-if (
-    "-v" in argv
-    or "--verbose" in argv
-    or os.environ.get("CLOUDKEEPER_VERBOSE", "False") == "True"
-):
-    getLogger("cloudkeeper").setLevel(DEBUG)
-
-
 log = getLogger("cloudkeeper")
 
 
 def add_args(arg_parser: ArgumentParser) -> None:
-    arg_parser.add_argument(
+    group = arg_parser.add_mutually_exclusive_group()
+    group.add_argument(
         "--verbose",
         "-v",
         help="Verbose logging",
@@ -36,8 +28,30 @@ def add_args(arg_parser: ArgumentParser) -> None:
         action="store_true",
         default=False,
     )
+    group.add_argument(
+        "--quiet",
+        help="Quiet logging",
+        dest="quiet",
+        action="store_true",
+        default=False,
+    )
 
 
 def setup_logger(proc: str) -> None:
-    log_format = f"%(asctime)s|{proc}|%(levelname)5s|%(process)d|%(threadName)10s  %(message)s"
+    log_format = (
+        f"%(asctime)s|{proc}|%(levelname)5s|%(process)d|%(threadName)10s  %(message)s"
+    )
     basicConfig(format=log_format, datefmt="%y-%m-%d %H:%M:%S")
+    argv = sys.argv[1:]
+    if (
+        "-v" in argv
+        or "--verbose" in argv
+        or os.environ.get("CLOUDKEEPER_VERBOSE", "false").lower() == "true"
+    ):
+        getLogger("cloudkeeper").setLevel(DEBUG)
+    elif (
+        "--quiet" in argv
+        or os.environ.get("CLOUDKEEPER_QUIET", "false").lower() == "true"
+    ):
+        getLogger().setLevel(WARNING)
+        getLogger("cloudkeeper").setLevel(CRITICAL)
