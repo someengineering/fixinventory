51bcffbe32222ca1f6f273269cd897fd00eb6d8e
Matthias Veit <matthias_veit@yahoo.de>
2021-11-25 08:58:40 +0100

    make mypy happy

--- ckcore/core/cli/command.py
+++ ckcore/core/cli/command.py
@@ -1554,22 +1554,22 @@ class ListCommand(CLICommand, OutputTransformer):
             as_name = path[-1] if prop == as_name or as_name is None else as_name
             props.append((path, as_name))
 
-        def fmt(elem: JsonElement) -> str:
-            result = ""
-            first = True
+        def fmt(elem: JsonElement) -> JsonElement:
             is_vertex = is_node(elem)
             if is_vertex and isinstance(elem, dict):
+                result = ""
+                first = True
                 for path, name in props:
                     value = value_in_path(elem, path)
                     if value is not None:
                         delim = "" if first else ", "
                         result += f"{delim}{to_str(name, value)}"
                         first = False
+                return result
             elif isinstance(elem, dict):
-                result = elem
+                return elem
             else:
-                result = str(elem)
-            return result
+                return str(elem)
 
         return CLIFlow(lambda in_stream: stream.map(in_stream, fmt))
 
