c346d00b0019cd68746df9f358dec6390b4c262a
Matthias Veit <aquamatthias@users.noreply.github.com>
2022-01-10 15:20:18 +0100

    [docs] Re-export diagrams with proper design (#522)

--- docs/source/cloud_resources/img/export_model_images.py
+++ docs/source/cloud_resources/img/export_model_images.py
@@ -1,26 +1,14 @@
 import requests
 
 core = "http://localhost:8900"
-
 kinds = requests.get(f"{core}/model").json()["kinds"]
-
-
-def bases_of(name: str):
-    kind = kinds[name]
-    bases = kind.get("bases", [])
-    result = list(bases)
-    for base in bases:
-        if base != name:
-            result.extend(bases_of(base))
-    return result
 
 
 def export_images(path: str):
     filter_out = ["aws", "gcp"]
     for name, kind in kinds.items():
-        if "bases" in kind and [a for a in filter_out if name.startswith(f"{a}_")]:
-            show = [name, *bases_of(name)]
-            image = requests.get(f"{core}/model/uml", params={"show": ",".join(show)})
+        if [a for a in filter_out if name.startswith(f"{a}_")]:
+            image = requests.get(f"{core}/model/uml", params={"show": name})
             with open(f"{path}/{name}.svg", "w+") as file:
                 file.write(image.text)
 
