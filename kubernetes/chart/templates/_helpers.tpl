60f9bc6cec855caed4a9380eebcb68c5c6bdb1c8
Matthias Veit <matthias_veit@yahoo.de>
2022-01-13 10:14:11 +0100

    [helm] cloudkeeper -> resoto

--- kubernetes/chart/templates/_helpers.tpl
+++ kubernetes/chart/templates/_helpers.tpl
@@ -1,7 +1,7 @@
 {{/*
 Expand the name of the chart.
 */}}
-{{- define "cloudkeeper.name" -}}
+{{- define "resoto.name" -}}
 {{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
@@ -10,7 +10,7 @@ Create a default fully qualified app name.
 We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
 If release name contains chart name it will be used as a full name.
 */}}
-{{- define "cloudkeeper.fullname" -}}
+{{- define "resoto.fullname" -}}
 {{- if .Values.fullnameOverride }}
 {{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
 {{- else }}
@@ -26,16 +26,16 @@ If release name contains chart name it will be used as a full name.
 {{/*
 Create chart name and version as used by the chart label.
 */}}
-{{- define "cloudkeeper.chart" -}}
+{{- define "resoto.chart" -}}
 {{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
 {{- end }}
 
 {{/*
 Common labels
 */}}
-{{- define "cloudkeeper.labels" -}}
-helm.sh/chart: {{ include "cloudkeeper.chart" . }}
-{{ include "cloudkeeper.selectorLabels" . }}
+{{- define "resoto.labels" -}}
+helm.sh/chart: {{ include "resoto.chart" . }}
+{{ include "resoto.selectorLabels" . }}
 {{- if .Chart.AppVersion }}
 app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
 {{- end }}
@@ -45,17 +45,17 @@ app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{/*
 Selector labels
 */}}
-{{- define "cloudkeeper.selectorLabels" -}}
-app.kubernetes.io/name: {{ include "cloudkeeper.name" . }}
+{{- define "resoto.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "resoto.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
 
 {{/*
 Create the name of the service account to use
 */}}
-{{- define "cloudkeeper.serviceAccountName" -}}
+{{- define "resoto.serviceAccountName" -}}
 {{- if .Values.serviceAccount.create }}
-{{- default (include "cloudkeeper.fullname" .) .Values.serviceAccount.name }}
+{{- default (include "resoto.fullname" .) .Values.serviceAccount.name }}
 {{- else }}
 {{- default "default" .Values.serviceAccount.name }}
 {{- end }}
--- kubernetes/setup-kind.sh
+++ kubernetes/setup-kind.sh
@@ -11,16 +11,16 @@ if [ -z "${NO_START_KIND}"]; then
   kind create cluster
 fi
 
-kubectl create ns cloudkeeper
+kubectl create ns resoto
 
 # deploy db operator.
-helm install --namespace cloudkeeper arango-crd https://github.com/arangodb/kube-arangodb/releases/download/1.2.4/kube-arangodb-crd-1.2.4.tgz
-helm install --namespace cloudkeeper arango https://github.com/arangodb/kube-arangodb/releases/download/1.2.4/kube-arangodb-1.2.4.tgz --set operator.replicaCount=1
+helm install --namespace resoto arango-crd https://github.com/arangodb/kube-arangodb/releases/download/1.2.4/kube-arangodb-crd-1.2.4.tgz
+helm install --namespace resoto arango https://github.com/arangodb/kube-arangodb/releases/download/1.2.4/kube-arangodb-1.2.4.tgz --set operator.replicaCount=1
 
 # wait for operator to be ready.
-kubectl --namespace cloudkeeper rollout status deploy/arango-arango-operator --timeout=300s
+kubectl --namespace resoto rollout status deploy/arango-arango-operator --timeout=300s
 # deploy a db.
-kubectl --namespace cloudkeeper apply -f - <<EOF
+kubectl --namespace resoto apply -f - <<EOF
 apiVersion: "database.arangodb.com/v1alpha"
 kind: "ArangoDeployment"
 metadata:
@@ -32,56 +32,56 @@ spec:
 EOF
 
 # create secret for dashboard. not really needed for test, but nice to have.
-kubectl --namespace cloudkeeper create secret generic arangodb-operator-dashboard --from-literal=username=a --from-literal=password=a
+kubectl --namespace resoto create secret generic arangodb-operator-dashboard --from-literal=username=a --from-literal=password=a
 
 # wait for the db deployment is ready.
-kubectl --namespace cloudkeeper wait --for=condition=ready arangodeployment/single-server --timeout=300s
+kubectl --namespace resoto wait --for=condition=ready arangodeployment/single-server --timeout=300s
 
 # get the db's pod.
-ARANGO_DB_POD=$(kubectl --namespace cloudkeeper get pod -larango_deployment=single-server -o name)
+ARANGO_DB_POD=$(kubectl --namespace resoto get pod -larango_deployment=single-server -o name)
 
 # wait until the db is ready to accept clients.
-timeout 1m $SHELL -c "until kubectl --namespace cloudkeeper exec $ARANGO_DB_POD -- /lifecycle/tools/arangodb_operator lifecycle probe --endpoint=/_api/version --auth; do sleep 1; done"
+timeout 1m $SHELL -c "until kubectl --namespace resoto exec $ARANGO_DB_POD -- /lifecycle/tools/arangodb_operator lifecycle probe --endpoint=/_api/version --auth; do sleep 1; done"
 
 # create a db and user for us.
-kubectl --namespace cloudkeeper exec -i $ARANGO_DB_POD -- arangosh --console.history false --server.password "" <<EOF
+kubectl --namespace resoto exec -i $ARANGO_DB_POD -- arangosh --console.history false --server.password "" <<EOF
 const users = require('@arangodb/users');
 print("creating user");
 users.save('ck', 'ck');
 print("creating db");
-db._createDatabase('cloudkeeper');
+db._createDatabase('resoto');
 print("granting user db access");
-users.grantDatabase('ck', 'cloudkeeper', 'rw');
+users.grantDatabase('ck', 'resoto', 'rw');
 print("all done:", users.all());
 EOF
 
 # put the db password in a secret.
-kubectl --namespace cloudkeeper create secret generic arango-user --from-literal=password=ck
+kubectl --namespace resoto create secret generic arango-user --from-literal=password=ck
 
 # install cloud keeper with the example collector
 
 DIR="$(dirname "$(realpath "$0")")"
-helm upgrade -i --namespace cloudkeeper cloudkeeper "$DIR/chart" --set image.tag=$IMAGE_TAG -f - <<EOF
-ckcore:
+helm upgrade -i --namespace resoto resoto "$DIR/chart" --set image.tag=$IMAGE_TAG -f - <<EOF
+resotocore:
   graphdb:
     server: http://single-server:8529
     login: ck
-    database: cloudkeeper
+    database: resoto
     passwordSecret:
       name: arango-user
       key: password
-ckworker:
+resotoworker:
   collector: example
   extraArgs:
     - --fork
 EOF
 # wait for it to be ready
-kubectl --namespace cloudkeeper rollout status deploy/cloudkeeper-ckcore --timeout=300s
-kubectl --namespace cloudkeeper rollout status deploy/cloudkeeper-ckworker --timeout=300s
-kubectl --namespace cloudkeeper rollout status deploy/cloudkeeper-ckmetrics --timeout=300s
+kubectl --namespace resoto rollout status deploy/resoto-resotocore --timeout=300s
+kubectl --namespace resoto rollout status deploy/resoto-resotoworker --timeout=300s
+kubectl --namespace resoto rollout status deploy/resoto-resotometrics --timeout=300s
 
 # see an example query!
 echo 'Setup done. You can now run queries. For example:'
-echo 'kubectl --namespace cloudkeeper exec -i deploy/cloudkeeper-ckcore -- cksh --stdin <<EOF'
+echo 'kubectl --namespace resoto exec -i deploy/resoto-resotocore -- resotoshell --stdin <<EOF'
 echo 'query is(resource) | count reported.kind'
 echo 'EOF'
