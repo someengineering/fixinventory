c8d933b44275a4ab8a4f01e070457b0363998ecc
Lukas Lösche <lukas@some.engineering>
2022-01-21 12:08:19 +0100

    Bump 2.0.0a12 (#565)

--- plugins/aws/setup.py
+++ plugins/aws/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-aws",
-    version="2.0.0a11",
+    version="2.0.0a12",
     description="Resoto AWS Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- plugins/gcp/setup.py
+++ plugins/gcp/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-gcp",
-    version="2.0.0a11",
+    version="2.0.0a12",
     description="Resoto GCP Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
