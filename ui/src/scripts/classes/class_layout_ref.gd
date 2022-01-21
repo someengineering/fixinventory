e742f7fc91e33ffdde2fa1782cb2d52a5ed4d038
Lukas Lösche <lukas@some.engineering>
2022-01-21 10:40:12 +0100

    Move UI to dedicated repository

--- ui/src/scripts/classes/class_layout_ref.gd
+++
@@ -1,8 +0,0 @@
-extends Reference
-class_name NodeLayoutRef
-
-var id: String = ""
-var position: Vector2
-#var next_pos : Vector2
-var velocity: Vector2
-var connections: Array = []
--- ui/src/scripts/tools/query_editor_highlights.gd
+++
@@ -1,5 +0,0 @@
-extends TextEdit
-
-
-func _ready():
-	add_keyword_color("and", Color("#f76526"))
