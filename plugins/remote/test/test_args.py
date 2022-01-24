dc550f25fe46815f33415d9c3a71716ec57dcaf8
Lukas Lösche <lukas@some.engineering>
2022-01-24 20:56:46 +0100

    Remove obsolete remote plugin

--- plugins/remote/test/test_args.py
+++
@@ -1,9 +0,0 @@
-from resotolib.args import get_arg_parser, ArgumentParser
-from resoto_plugin_remote import RemotePlugin
-
-
-def test_args():
-    arg_parser = get_arg_parser()
-    RemotePlugin.add_args(arg_parser)
-    arg_parser.parse_args()
-    assert len(ArgumentParser.args.remote_endpoint) == 0
