d9409de4929069320d8c7accdc80a2c11b176af4
Lukas Lösche <lukas@some.engineering>
2022-01-29 00:13:04 +0100

    Bump 2.0.0a14 (#597)
    * Bump 2.0.0a14
    
    Co-authored-by: TheCatLady <52870424+TheCatLady@users.noreply.github.com>

--- plugins/vsphere/setup.py
+++ plugins/vsphere/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-vsphere",
-    version="2.0.0a13",
+    version="2.0.0a14",
     description="Resoto VSphere Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
