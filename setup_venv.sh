e2a67116bc987df26b54b79a6dc1c35c819c5534
Nikita Melkozerov <nikita@some.engineering>
2022-02-11 14:32:23 +0100

    fix the docker bulid

--- setup_venv.sh
+++ setup_venv.sh
@@ -179,7 +179,7 @@ install_resoto() {
 }
 
 install_plugins() {
-    local collector_plugins=(aws gcp slack onelogin k8s onprem github example_collector cleanup_expired cleanup_aws_alarms cleanup_aws_loadbalancers cleanup_aws_vpcs cleanup_untagged cleanup_volumes protect_snowflakes tagvalidator)
+    local collector_plugins=(aws gcp slack onelogin k8s onprem github example_collector cleanup_expired cleanup_aws_alarms cleanup_aws_loadbalancers cleanup_aws_vpcs cleanup_untagged cleanup_volumes protect_snowflakes tagvalidator vsphere)
     for plugin in "${collector_plugins[@]}"; do
         pip_install "$plugin" true
     done
