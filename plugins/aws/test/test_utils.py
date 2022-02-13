cbee35815049aaa9372ac20b13f7516c8573f819
Lukas Lösche <lukas@some.engineering>
2022-02-13 22:39:48 +0100

    [plugins/aws][feat] Construct S3 ARN

---
+++ plugins/aws/test/test_utils.py
@@ -0,0 +1,11 @@
+from resoto_plugin_aws.resources import AWSRegion
+from resoto_plugin_aws.utils import arn_partition
+
+
+def test_arn_partition():
+    us_east_1 = AWSRegion("us-east-1")
+    cn_north_1 = AWSRegion("cn-north-1")
+    us_gov_east_1 = AWSRegion("us-gov-east-1")
+    assert arn_partition(us_east_1) == "aws"
+    assert arn_partition(cn_north_1) == "aws-cn"
+    assert arn_partition(us_gov_east_1) == "aws-us-gov"
