88a820ba738ee7cf96d3c2856168ebc17c22b176
Lukas Lösche <lukas@some.engineering>
2022-02-15 03:23:56 +0100

    Update tests

--- resotolib/test/test_graph_extensions.py
+++ resotolib/test/test_graph_extensions.py
@@ -10,7 +10,7 @@ from resotolib.graph.graph_extensions import dependent_node_iterator
 def graph() -> DiGraph:
     g = DiGraph()
     for i in range(1, 14):
-        g.add_node(i, id=i)
+        g.add_node(i)
     g.add_edges_from([(1, 2), (1, 3), (2, 3)])  # island 1
     g.add_edges_from([(4, 5), (4, 6), (6, 7)])  # island 2
     g.add_edges_from(
@@ -21,7 +21,7 @@ def graph() -> DiGraph:
 
 def test_reversed_directed_traversal(graph: DiGraph):
     def nodes(*ls: int) -> List[Dict[str, int]]:
-        return [{"id": e} for e in ls]
+        return [e for e in ls]
 
     result = list(dependent_node_iterator(graph))
     assert len(result) == 3  # 3 steps to complete
@@ -36,7 +36,7 @@ def test_delete_nodes(graph: DiGraph):
     to_delete = graph.copy()
     for parallel in dependent_node_iterator(graph):
         for node in parallel:
-            to_delete.remove_node(node["id"])
+            to_delete.remove_node(node)
     assert len(to_delete.nodes) == 0
 
 
