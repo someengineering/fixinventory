9f0d332319a783b2a6caf29746b590daae6117b4
Matthias Veit <matthias_veit@yahoo.de>
2022-03-08 14:42:34 +0100

    fix tests

--- plugins/digitalocean/test/test_args.py
+++ plugins/digitalocean/test/test_args.py
@@ -6,4 +6,4 @@ def test_args():
     arg_parser = get_arg_parser()
     DigitalOceanCollectorPlugin.add_args(arg_parser)
     arg_parser.parse_args()
-    assert ArgumentParser.args.digitalocean_region is None
+    assert ArgumentParser.args.digitalocean_api_tokens == []
