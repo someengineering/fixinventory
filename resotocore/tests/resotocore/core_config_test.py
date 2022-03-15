f38519c63e39a082903c67e3a754b1cb257ca584
Matthias Veit <matthias_veit@yahoo.de>
2022-03-15 20:14:26 +0100

    fix environment settings

--- resotocore/tests/resotocore/core_config_test.py
+++ resotocore/tests/resotocore/core_config_test.py
@@ -14,7 +14,7 @@ from resotocore.dependencies import parse_args
 
 
 def test_parse_empty(default_config: CoreConfig) -> None:
-    result = parse_config(parse_args([]), {})
+    result = parse_config(parse_args(["--analytics-opt-out"]), {})
     assert result == default_config
 
 
@@ -25,7 +25,7 @@ def test_read_config() -> None:
             "cli": {"default_graph": "foo", "default_section": "bla"},
             "graph_update": {"abort_after_seconds": 1234, "merge_max_wait_time_seconds": 4321},
             "runtime": {
-                "analytics_opt_out": False,
+                "analytics_opt_out": True,
                 "debug": True,
                 "log_level": "WARN",
                 "plantuml_server": "https://foo",
@@ -33,7 +33,7 @@ def test_read_config() -> None:
             },
         }
     }
-    parsed = parse_config(parse_args([]), config)
+    parsed = parse_config(parse_args(["--analytics-opt-out"]), config)
     assert parsed.json() == config
 
 
@@ -78,5 +78,6 @@ def default_config() -> CoreConfig:
             plantuml_server="http://plantuml.resoto.org:8080",
             start_collect_on_subscriber_connect=False,
         ),
-        args=parse_args([]),
+        # We use this flag explicitly - otherwise it is picked up by env vars
+        args=parse_args(["--analytics-opt-out"]),
     )
