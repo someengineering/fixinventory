1d241aed496765535a70d9169d98ded036b0d6c6
Lukas Lösche <lukas@some.engineering>
2022-03-22 07:08:04 +0100

    Add config test

---
+++ resotolib/test/test_config.py
@@ -0,0 +1,34 @@
+import pytest
+from dataclasses import dataclass, field
+from typing import Optional, ClassVar
+from resotolib.config import Config, ConfigNotFoundError
+from resotolib.args import get_arg_parser
+from resotolib.core import add_args as core_add_args
+
+
+def test_config():
+    arg_parser = get_arg_parser()
+    core_add_args(arg_parser)
+    arg_parser.parse_args()
+    cfg = Config("test")
+    cfg.add_config(TestConfig)
+    cfg.init_default_config()
+    cfg2 = Config("test2")
+    assert cfg.configtest.testvar1 == cfg2.configtest.testvar1
+    assert cfg.configtest.testvar1 == Config.configtest.testvar1
+    assert cfg.configtest.testvar2 == cfg2.configtest.testvar2
+    Config.configtest.testvar2 += 1
+    assert cfg.configtest.testvar2 == 12346
+    with pytest.raises(ConfigNotFoundError) as e:
+        Config.does_not_exist.foo = "bar"
+
+
+@dataclass
+class TestConfig:
+    kind: ClassVar[str] = "configtest"
+    testvar1: Optional[str] = field(
+        default="testing123", metadata={"description": "A test string"}
+    )
+    testvar2: Optional[int] = field(
+        default=12345, metadata={"description": "A test integer"}
+    )
