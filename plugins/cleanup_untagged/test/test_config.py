97b48ad00f5d33f7a6050142cbea2d531b57898f
Lukas Lösche <lukas@some.engineering>
2022-03-24 18:38:40 +0100

    Add cleanup_untagged Config

---
+++ plugins/cleanup_untagged/test/test_config.py
@@ -0,0 +1,13 @@
+from resotolib.config import Config
+from resoto_plugin_cleanup_untagged import CleanupUntaggedPlugin
+
+
+def test_config():
+    config = Config("dummy", "dummy")
+    CleanupUntaggedPlugin.add_config(config)
+    Config.init_default_config()
+    assert Config.plugin_cleanup_untagged.enabled is False
+    assert (
+        Config.plugin_cleanup_untagged.validate(Config.plugin_cleanup_untagged.config)
+        is True
+    )
