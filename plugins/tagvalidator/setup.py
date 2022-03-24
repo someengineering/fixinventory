862c3d85bd6ac185c29d9a699b775032ba356b43
Lukas Lösche <lukas@some.engineering>
2022-03-24 16:14:36 +0100

    [resoto][chore] Bump 2.0.0a20 (#730)

--- plugins/tagvalidator/setup.py
+++ plugins/tagvalidator/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-tagvalidator",
-    version="2.0.0a19",
+    version="2.0.0a20",
     description="Resoto Tag Validator Plugin",
     license="Apache 2.0",
     packages=find_packages(),
