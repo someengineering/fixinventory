4745e98ae06a03ffdf94b9a02554cba7a7e4cee2
Lukas Lösche <lukas@some.engineering>
2022-03-28 14:59:43 +0200

    [resoto][chore] Bump 2.0.0a21

--- plugins/slack/setup.py
+++ plugins/slack/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-slack",
-    version="2.0.0a20",
+    version="2.0.0a21",
     description="Resoto Slack Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- resotocore/resotocore/__init__.py
+++ resotocore/resotocore/__init__.py
@@ -11,7 +11,7 @@ __description__ = "resoto core."
 __author__ = "Some Engineering Inc."
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright © 2022 Some Engineering Inc."
-__version__ = "2.0.0a20"
+__version__ = "2.0.0a21"
 
 
 def version() -> str:
