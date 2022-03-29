4951b1a6ff4f343a1cab895d12d309b3f989eade
Nikita Melkozerov <n.melkozerov@gmail.com>
2022-03-29 13:39:31 +0200

    [resoto][chore] Bump 2.0.0a22 (#744)

--- plugins/k8s/setup.py
+++ plugins/k8s/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-k8s",
-    version="2.0.0a21",
+    version="2.0.0a22",
     description="Resoto Kubernetes Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
