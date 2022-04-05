d0618004d2797ac2aab7d74c4372ef8acf1383b5
Lukas Lösche <lukas@some.engineering>
2022-04-05 19:20:20 +0200

    Bump 2.0.0rc0

--- plugins/github/setup.py
+++ plugins/github/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-github",
-    version="2.0.0a22",
+    version="2.0.0rc0",
     description="Resoto Github Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- resotocore/setup.py
+++ resotocore/setup.py
@@ -22,7 +22,7 @@ setup_requirements = [
 
 setup(
     name="resotocore",
-    version="2.0.0a22",
+    version="2.0.0rc0",
     description="Keeps all the things.",
     python_requires=">=3.5",
     classifiers=["Programming Language :: Python :: 3"],
