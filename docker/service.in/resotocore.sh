ba2e4a1c368c97a358772a83403bf546a0348549
Lukas Lösche <lukas@some.engineering>
2022-04-05 20:55:51 +0200

    [docker][fix] Update Dockerfile(s) for config service and tls (#764)

--- docker/service.in/resotocore.sh
+++ docker/service.in/resotocore.sh
@@ -5,4 +5,4 @@ if [ "$START_GRAPHDB" = true ]; then
     sleep 7
 fi
 source /usr/local/resoto-venv-pypy3/bin/activate
-exec /sbin/setuser resoto resotocore --ui-path /usr/local/resoto/ui/ --start-collect-on-subscriber-connect @RESOTO_ARGSDISPATCHER@
+exec /sbin/setuser resoto resotocore --override resotocore.api.ui_path=/usr/local/resoto/ui/ resotocore.runtime.start_collect_on_subscriber_connect=true resotocore.api.web_hosts=0.0.0.0 @RESOTO_ARGSDISPATCHER@
