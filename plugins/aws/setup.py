f73c592eea9ee739135eda69156d96a9a14d45e9
Lukas Lösche <lukas@some.engineering>
2022-04-05 19:24:41 +0200

    Bump 2.0.0rc1

--- plugins/aws/setup.py
+++ plugins/aws/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-aws",
-    version="2.0.0rc0",
+    version="2.0.0rc1",
     description="Resoto AWS Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- plugins/onelogin/setup.py
+++ plugins/onelogin/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-onelogin",
-    version="2.0.0rc0",
+    version="2.0.0rc1",
     description="Resoto OneLogin Plugin",
     license="Apache 2.0",
     packages=find_packages(),
