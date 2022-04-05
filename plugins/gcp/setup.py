c533be291c81f4bce0e1374a23de12defd0f726a
Lukas Lösche <lukas@some.engineering>
2022-04-05 19:21:52 +0200

    [resoto][chore] Bump 2.0.0rc0 (#762)

--- plugins/gcp/setup.py
+++ plugins/gcp/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-gcp",
-    version="2.0.0a22",
+    version="2.0.0rc0",
     description="Resoto GCP Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
