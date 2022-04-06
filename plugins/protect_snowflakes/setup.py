6dd1ebc7496d85e55e22ec59fe0cb2851deea427
Lukas Lösche <lukas@some.engineering>
2022-04-06 04:51:25 +0200

    Bump 2.0.0rc1 -> 2.0.0rc2

--- plugins/protect_snowflakes/setup.py
+++ plugins/protect_snowflakes/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-protect_snowflakes",
-    version="2.0.0rc1",
+    version="2.0.0rc2",
     description="Resoto Snowflake Protection Plugin",
     license="Apache 2.0",
     packages=find_packages(),
