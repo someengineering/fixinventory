61acf0dd52c379b9b21a6b9e4539cc08f41935a6
Lukas Lösche <lukas@some.engineering>
2022-04-12 20:45:20 +0200

    Bump 2.1.0a0

--- plugins/aws/setup.py
+++ plugins/aws/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-aws",
-    version="2.0.0",
+    version="2.1.0a0",
     description="Resoto AWS Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- plugins/cleanup_volumes/setup.py
+++ plugins/cleanup_volumes/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-cleanup_volumes",
-    version="2.0.0",
+    version="2.1.0a0",
     description="Volume Cleaner Plugin",
     license="Apache 2.0",
     packages=find_packages(),
