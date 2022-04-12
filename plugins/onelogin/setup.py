a80aa2ede17e9803e3958364d5e9857026e877db
Lukas Lösche <lukas@some.engineering>
2022-04-12 20:35:39 +0200

    Bump 2.0.0

--- plugins/onelogin/setup.py
+++ plugins/onelogin/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-onelogin",
-    version="2.0.0rc3",
+    version="2.0.0",
     description="Resoto OneLogin Plugin",
     license="Apache 2.0",
     packages=find_packages(),
