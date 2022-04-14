ca645ba09af97eb3fbe3e461e3d9eb6bccd35338
Matthias Veit <matthias_veit@yahoo.de>
2022-04-14 14:39:38 +0200

    use version 2.0.1

--- plugins/cleanup_expired/setup.py
+++ plugins/cleanup_expired/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-cleanup_expired",
-    version="2.0.0",
+    version="2.0.1",
     description="Resoto Expired Resource Cleanup Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- plugins/onelogin/setup.py
+++ plugins/onelogin/setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-onelogin",
-    version="2.0.0",
+    version="2.0.1",
     description="Resoto OneLogin Plugin",
     license="Apache 2.0",
     packages=find_packages(),
