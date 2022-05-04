b8f2f06c9aff54ab651072b4a1438d05bf34291f
Matthias Veit <matthias_veit@yahoo.de>
2022-05-04 17:15:04 +0200

    fix test

--- resotocore/tests/resotocore/web/api_test.py
+++ resotocore/tests/resotocore/web/api_test.py
@@ -290,7 +290,7 @@ async def test_cli(core_client: ApiClient) -> None:
 
     # list all cli commands
     info = AccessJson(core_client.cli_info())
-    assert len(info.commands) == 34
+    assert len(info.commands) == 35
 
 
 @pytest.mark.asyncio
