637c4eea3bcab96c0b95012cff65e7b9229ec6da
Lukas Lösche <lukas@some.engineering>
2022-05-13 16:45:12 +0200

    [resoto][chore] Bump 2.3.0a0 (#864)

--- plugins/k8s/setup.py
+++ plugins/k8s/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-k8s",
-    version="2.2.0a0",
+    version="2.3.0a0",
     description="Resoto Kubernetes Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
