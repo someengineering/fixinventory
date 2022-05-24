5208b696d4e8874b175a7c78ba6e82a7bd41b72f
Matthias Veit <matthias_veit@yahoo.de>
2022-05-24 12:14:57 +0200

    add a simple web based log viewer

---
+++ resotolog/resotolog/ui/precache-manifest.fd28897168b92abb01811781d22bd6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d329f0b60906c3464199676ea62fc3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed1e85fc072e4f8b6866",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3162dd06dbf5bfb420a6",
+    "url": "./static/js/2.a74a8517.chunk.js"
+  },
+  {
+    "revision": "ed1e85fc072e4f8b6866",
+    "url": "./static/js/main.040bcf2d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
