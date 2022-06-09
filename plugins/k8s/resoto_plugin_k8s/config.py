70d03a154721fd20ee0a3ef9867bf3283492e517
Matthias Veit <matthias_veit@yahoo.de>
2022-06-09 12:33:52 +0200

    [k8s][feat] Configure access directly or via file

--- plugins/k8s/resoto_plugin_k8s/config.py
+++ plugins/k8s/resoto_plugin_k8s/config.py
@@ -1,24 +1,102 @@
 import logging
 from dataclasses import dataclass, field
-from typing import List, ClassVar, Optional, Dict
+from textwrap import dedent
+from typing import List, ClassVar, Dict
 
-from kubernetes import client, config
 from kubernetes.client import Configuration
+from kubernetes.config import load_kube_config, list_kube_config_contexts
+
 from resotolib.utils import num_default_threads
 
 log = logging.getLogger("resoto." + __name__)
 
 
 @dataclass
+class K8sAccess:
+    kind: ClassVar[str] = "k8s_access"
+    name: str = field(metadata={"description": "The name of the kubernetes cluster."})
+    certificate_authority_data: str = field(metadata={"description": "The CA certificate string."})
+    server: str = field(metadata={"description": "The url of the server to connect to."})
+    token: str = field(metadata={"description": "The user access token to use to access this cluster."})
+
+    def to_yaml(self) -> str:
+        return dedent(
+            f"""
+             apiVersion: v1
+             clusters:
+             - cluster:
+                 certificate-authority-data: {self.certificate_authority_data}
+                 server: {self.server}
+               name: {self.name}
+             contexts:
+             - context:
+                 cluster: {self.name}
+                 user: access-{self.name}
+                 namespace: resoto
+               name: {self.name}
+             current-context: {self.name}
+             kind: Config
+             preferences: {{}}
+             users:
+             - name: access-{self.name}
+               user:
+                 token: {self.token}
+        """
+        )
+
+
+@dataclass
+class K8sConfigFile:
+    kind: ClassVar[str] = "k8s_config_file"
+    path: str = field(metadata={"description": "Path to the kubeconfig file."})
+    contexts: List[str] = field(
+        default_factory=list,
+        metadata={
+            "description": "The contexts to use in the specified config file.\n"
+            "You can also set all_contexts to true to use all contexts."
+        },
+    )
+    all_contexts: bool = field(
+        default=True,
+        metadata={"description": "Collect all contexts found in the kubeconfig file."},
+    )
+
+
+@dataclass
 class K8sConfig:
-    # TODO: replicate the kubernetes config file structure
     kind: ClassVar[str] = "k8s"
-    context: List[str] = field(default_factory=list, metadata={"description": "Context(s)"})
-    config: Optional[str] = field(default=None, metadata={"description": "Config file(s)"})
-    cluster: List[str] = field(default_factory=list, metadata={"description": "Cluster name(s)"})
-    apiserver: List[str] = field(default_factory=list, metadata={"description": "API Server(s)"})
-    token: List[str] = field(default_factory=list, metadata={"description": "Token(s)"})
-    cacert: List[str] = field(default_factory=list, metadata={"description": "CA certificate(s)"})
+    configs: List[K8sAccess] = field(
+        default_factory=list,
+        metadata={
+            "description": dedent(
+                """
+                Configure access to k8s clusters.
+                Structure:
+                - name: 'k8s-cluster-name'
+                  certificate_authority_data: 'CERT'
+                  server: 'https://k8s-cluster-server.example.com'
+                  token: 'TOKEN'
+                """
+            ).strip()
+        },
+    )
+
+    config_files: List[K8sConfigFile] = field(
+        default_factory=list,
+        metadata={
+            "description": dedent(
+                """
+                Configure access via kubeconfig files.
+                Structure:
+                Structure:
+                  - path: "/path/to/kubeconfig"
+                    all_contexts: false
+                    contexts: ["context1", "context2"]
+                """
+            ).strip()
+        },
+    )
+
     collect: List[str] = field(
         default_factory=list,
         metadata={"description": "Objects to collect (default: all)"},
@@ -35,78 +113,29 @@ class K8sConfig:
         default=False,
         metadata={"description": "Fork collector process instead of using threads"},
     )
-    all_contexts: bool = field(
-        default=False,
-        metadata={"description": "Collect all contexts in kubeconfig file"},
-    )
 
     def is_allowed(self, kind: str) -> bool:
         return (not self.collect or kind in self.collect) and kind not in self.no_collect
 
-    def cluster_access_configs(self) -> Dict[str, Configuration]:
-        cfg = {}
-        num_k8s_clusters = len(self.cluster)
-        num_k8s_apiserver = len(self.apiserver)
-        num_k8s_token = len(self.token)
-        num_k8s_cacerts = len(self.cacert)
+    def cluster_access_configs(self, tmp_dir: str) -> Dict[str, Configuration]:
+        result = {}
+        cfg_files = self.config_files
 
-        if not (num_k8s_clusters == num_k8s_token == num_k8s_apiserver == num_k8s_cacerts):
-            log_msg = (
-                f"Number of K8S clusters ({num_k8s_clusters}), API servers"
-                f" ({num_k8s_apiserver}), CA Certs ({num_k8s_cacerts}) and tokens"
-                f" ({num_k8s_token}) not equal."
-            )
-            raise RuntimeError(log_msg)
+        # write all access configs as kubeconfig file and let the loader handle it
+        for ca in self.configs:
+            filename = tmp_dir + "/" + ca.name + ".yaml"
+            with open(filename, "w") as f:
+                f.write(ca.to_yaml())
+            cfg_files.append(K8sConfigFile(path=filename))
 
-        if len(self.cluster) != len(set(self.cluster)):
-            log_msg = "List of Kubernetes clusters contains duplicate entries"
-            raise RuntimeError(log_msg)
+        # load all kubeconfig files
+        for cf in cfg_files:
+            all_contexts, active_context = list_kube_config_contexts(cf.path)
+            contexts = all_contexts if cf.all_contexts else [a for a in all_contexts if a["name"] in cf.contexts]
+            for ctx in contexts:
+                name = ctx["name"]
+                config = Configuration()
+                load_kube_config(cf.path, name, client_configuration=config)
+                result[name] = config
 
-        cluster_context_conflicts = set(self.context).intersection(set(self.cluster))
-        if len(cluster_context_conflicts) != 0:
-            log_msg = "Kubernetes cluster name(s) conflict with context(s):" f" {', '.join(cluster_context_conflicts)}"
-            raise RuntimeError(log_msg)
-
-        try:
-            contexts, active_context = config.list_kube_config_contexts(config_file=self.config)
-        except config.config_exception.ConfigException as e:
-            log.error(e)
-        else:
-            if contexts:
-                if self.all_contexts:
-                    log.debug("importing all contexts in configuration file since --k8s-all-contexts was specified")
-                elif len(self.context) == 0 and len(self.cluster) == 0:
-                    active_context = active_context["name"]
-                    log.debug(
-                        (
-                            "no --k8s-context or --k8s-cluster specified, defaulting to"
-                            f" active context {active_context}. To import all contexts"
-                            " in configuration file, use --k8s-all-contexts"
-                        )
-                    )
-                else:
-                    active_context = None
-
-                contexts = [context["name"] for context in contexts]
-
-                for context in contexts:
-                    if not self.all_contexts and context not in self.context and context != active_context:
-                        log.debug(f"skipping context {context} as it is not specified" " in --k8s-context")
-                        continue
-                    log.debug(f"loading context {context}")
-                    k8s_cfg = client.Configuration()
-                    config.load_kube_config(
-                        context=context,
-                        client_configuration=k8s_cfg,
-                        config_file=self.config,
-                    )
-                    cfg[context] = k8s_cfg
-
-        for idx, cluster in enumerate(self.cluster):
-            k8s_cfg = client.Configuration()
-            k8s_cfg.host = self.apiserver[idx]
-            k8s_cfg.api_key = {"authorization": "Bearer " + self.token[idx]}
-            k8s_cfg.ssl_ca_cert = self.cacert[idx]
-            cfg[cluster] = k8s_cfg
-
-        return cfg
+        return result
