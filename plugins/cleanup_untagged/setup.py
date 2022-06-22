91f654580956d32de8f03690b76273c7257b9173
Matthias Veit <aquamatthias@users.noreply.github.com>
2022-06-22 17:04:11 +0200

    [resoto][chore] Bump next release version. (#945)

--- plugins/cleanup_untagged/setup.py
+++ plugins/cleanup_untagged/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-cleanup-untagged",
-    version="2.3.0a0",
+    version="2.4.0a0",
     description="Resoto Cleanup Untagged Plugin",
     license="Apache 2.0",
     packages=find_packages(),
