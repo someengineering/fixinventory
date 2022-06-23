1a5875db6da51a62fa0f396347d5ca585237a603
Lukas Lösche <lukas@some.engineering>
2022-06-23 15:12:04 +0200

    load with proper type (#950)
    Co-authored-by: Matthias Veit <matthias_veit@yahoo.de>

--- resotometrics/test/test_args.py
+++ resotometrics/test/test_args.py
@@ -1,5 +1,6 @@
 from resotolib.args import ArgumentParser
 from resotolib.core import resotocore, add_args
+from resotometrics.config import _load_default_metrics, MetricType
 
 
 def test_args():
@@ -8,3 +9,10 @@ def test_args():
     arg_parser.parse_args()
     assert resotocore.http_uri == "https://localhost:8900"
     assert resotocore.ws_uri == "wss://localhost:8900"
+
+
+def test_default_config() -> None:
+    metrics = _load_default_metrics()
+    assert len(metrics) > 1
+    first = next(iter(metrics.values()))
+    assert isinstance(first.type, MetricType)
