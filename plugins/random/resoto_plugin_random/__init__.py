3d42ac77419f3ea83c934c104ba8efac61cfe023
Lukas Lösche <lukas@some.engineering>
2022-06-30 16:20:11 +0200

    Add owner tag for most instances

--- plugins/random/resoto_plugin_random/__init__.py
+++ plugins/random/resoto_plugin_random/__init__.py
@@ -1,5 +1,7 @@
+from lib2to3.pytree import Base
 import random
 import hashlib
+from resotolib.baseresources import BaseResource
 from resotolib.logger import log
 from resotolib.baseplugin import BaseCollectorPlugin
 from resotolib.graph import Graph
@@ -7,12 +9,14 @@ from resotolib.args import ArgumentParser
 from resotolib.config import Config
 from .config import RandomConfig
 from .resources import (
+    first_names,
     RandomAccount,
     RandomRegion,
     RandomNetwork,
     RandomInstance,
     RandomVolume,
 )
+from typing import List, Callable, Dict, Optional
 
 
 class RandomCollectorPlugin(BaseCollectorPlugin):
@@ -41,64 +45,185 @@ class RandomCollectorPlugin(BaseCollectorPlugin):
         config.add_config(RandomConfig)
 
 
-regions = {
-    "ap-northeast-1": "Asia Pacific (Tokyo)",
-    "ap-southeast-2": "Asia Pacific (Sydney)",
-    "ap-southeast-1": "Asia Pacific (Singapore)",
-    "ap-northeast-2": "Asia Pacific (Seoul)",
-    "ap-south-1": "Asia Pacific (Mumbai)",
-    "ca-central-1": "Canada (Central)",
-    "eu-central-1": "EU (Frankfurt)",
-    "eu-west-1": "EU (Ireland)",
-    "eu-west-2": "EU (London)",
-    "eu-west-3": "EU (Paris)",
-    "eu-north-1": "EU (Stockholm)",
-    "sa-east-1": "South America (São Paulo)",
-    "us-east-1": "US East (N. Virginia)",
-    "us-east-2": "US East (Ohio)",
-    "us-west-1": "US West (N. California)",
-    "us-west-2": "US West (Oregon)",
-}
-
-
 def get_id(input: str, digest_size: int = 10) -> str:
     return hashlib.blake2b(str(input).encode(), digest_size=digest_size).digest().hex()
 
 
 def add_random_resources(graph: Graph) -> None:
-    region_list = list(regions)
+    add_accounts(graph)
+
+
+def add_accounts(graph: Graph) -> None:
     num_accounts = random.randint(1, 10)
     log.debug(f"Adding {num_accounts} accounts")
     for account_num in range(num_accounts):
         account_id = str(int(get_id(f"account_{account_num}", 6), 16))
         account = RandomAccount(account_id, {}, name=f"Random Account {account_num}")
         graph.add_resource(graph.root, account)
-        num_regions = random.randint(0, len(regions))
-        log.debug(f"Adding {num_regions} regions in {account.rtdname}")
-        for region_num in range(num_regions):
-            region = RandomRegion(region_list[region_num], {}, name=regions[region_list[region_num]], _account=account)
-            graph.add_resource(account, region)
-            num_networks = random.randint(1, 20)
-            log.debug(f"Adding {num_networks} networks in {account.rtdname} {region.rtdname}")
-            for network_num in range(num_networks):
-                network_id = "net-" + get_id(f"network_{account_num}_{region_num}_{network_num}")
-                network = RandomNetwork(network_id, {}, _account=account, _region=region)
-                graph.add_resource(region, network)
-                num_instances = random.randint(1, 500)
-                log.debug(f"Adding {num_instances} instances in {account.rtdname} {region.rtdname} {network.rtdname}")
-                for instance_num in range(num_instances):
-                    instance_id = "i-" + get_id(f"instance_{account_num}_{region_num}_{network_num}_{instance_num}")
-                    instance = RandomInstance(instance_id, {}, _account=account, _region=region)
-                    graph.add_resource(region, instance)
-                    graph.add_edge(network, instance)
-                    num_volumes = random.randint(1, 5)
-                    log.debug(
-                        f"Adding {num_volumes} volumes to {account.rtdname} {region.rtdname} {network.rtdname} {instance.rtdname}"
-                    )
-                    for volume_num in range(num_volumes):
-                        volume_id = "vol-" + get_id(
-                            f"volume_{account_num}_{region_num}_{network_num}_{instance_num}_{volume_num}"
-                        )
-                        volume = RandomVolume(volume_id, {}, _account=account, _region=region)
-                        graph.add_resource(region, volume)
-                        graph.add_edge(instance, volume)
+        add_regions(graph, [account], account=account)
+
+
+region_templates = {
+    "ap-northeast-": "Asia Pacific North East",
+    "ap-southeast-": "Asia Pacific South East",
+    "ap-south-": "Asia Pacific South",
+    "ca-central-": "Canada Central",
+    "eu-central-": "EU Central",
+    "eu-north-": "EU North",
+    "eu-west-": "EU West",
+    "sa-east-": "South America East",
+    "us-east-": "US East",
+    "us-west-": "US West",
+}
+
+
+def add_regions(graph: Graph, parents: List[BaseResource], account: BaseResource = None) -> None:
+    num_total_regions = random.randint(10, 100)
+    all_regions = {}
+    r_num = 1
+    i = 0
+    while i < num_total_regions:
+        for rt_short, rt_long in region_templates.items():
+            r_short = f"{rt_short}{r_num}"
+            r_long = f"{rt_long} {r_num}"
+            all_regions[r_short] = r_long
+            i += 1
+            if i >= num_total_regions:
+                break
+        r_num += 1
+
+    num_regions = random.randint(1, 4)
+    regions = random.sample(sorted(all_regions), num_regions)
+    log.debug(f"Adding {num_regions} regions {regions} in {account.rtdname}")
+    for r in regions:
+        region = RandomRegion(r, {}, name=all_regions[r], _account=account)
+        graph.add_node(region)
+        for parent in parents:
+            graph.add_edge(parent, region)
+        id_path = f"{account.id}/{region.id}"
+        add_networks(graph, [region], account=account, region=region, id_path=id_path)
+
+
+def add_networks(
+    graph: Graph, parents: List[BaseResource], id_path: str, account: BaseResource = None, region: BaseResource = None
+) -> None:
+    add_resources(
+        graph=graph,
+        parents=parents,
+        children=[add_instances],
+        cls=RandomNetwork,
+        short_prefix="net-",
+        long_prefix="Network",
+        min=1,
+        max=4,
+        id_path=id_path,
+        account=account,
+        region=region,
+    )
+
+
+instance_statuses = ["pending", "running", "shutting-down", "terminated", "stopping", "stopped"]
+instance_types = {
+    "rnd2.tiny": [2, 2],
+    "rnd2.micro": [2, 4],
+    "rnd2.medium": [4, 8],
+    "rnd2.large": [8, 16],
+    "rnd2.xlarge": [8, 32],
+    "rnd2.2xlarge": [16, 64],
+    "rnd2.mega": [32, 128],
+    "rnd2.ultra": [64, 256],
+}
+
+employees = random.choices(first_names, k=random.randint(5, 30))
+
+
+def add_instances(
+    graph: Graph, parents: List[BaseResource], id_path: str, account: BaseResource = None, region: BaseResource = None
+) -> None:
+    instance_status = random.choices(instance_statuses, weights=[1, 85, 1, 11, 1, 1], k=1)[0]
+    instance_type = random.choices(list(instance_types), weights=[10, 10, 20, 50, 20, 10, 5, 5], k=1)[0]
+    tags = {}
+    has_owner = random.randrange(100) < 90
+    if has_owner:
+        tags["owner"] = random.choice(employees)
+    kwargs = {
+        "tags": tags,
+        "instance_status": instance_status,
+        "instance_type": instance_type,
+        "instance_cores": instance_types[instance_type][0],
+        "instance_memory": instance_types[instance_type][1],
+    }
+    add_resources(
+        graph=graph,
+        parents=parents,
+        children=[add_volumes],
+        cls=RandomInstance,
+        short_prefix="i-",
+        long_prefix="Instance",
+        min=0,
+        max=100,
+        id_path=id_path,
+        account=account,
+        region=region,
+        kwargs=kwargs,
+    )
+
+
+volume_statuses = ["creating", "available", "in-use", "deleting", "deleted", "error"]
+
+
+def add_volumes(
+    graph: Graph, parents: List[BaseResource], id_path: str, account: BaseResource = None, region: BaseResource = None
+) -> None:
+    volume_status = random.choices(volume_statuses, weights=[2, 15, 80, 1, 1, 1], k=1)[0]
+    tags = {}
+    kwargs = {"tags": tags, "volume_status": volume_status}
+    add_resources(
+        graph=graph,
+        parents=parents,
+        children=[],
+        cls=RandomVolume,
+        short_prefix="vol-",
+        long_prefix="Volume",
+        min=1,
+        max=5,
+        id_path=id_path,
+        account=account,
+        region=region,
+        kwargs=kwargs,
+    )
+
+
+def add_resources(
+    graph: Graph,
+    parents: List[BaseResource],
+    children: List[Callable],
+    cls: BaseResource,
+    short_prefix: str,
+    long_prefix: str,
+    min: int,
+    max: int,
+    id_path: str,
+    account: BaseResource = None,
+    region: BaseResource = None,
+    kwargs: Optional[Dict] = None,
+) -> None:
+    if kwargs is None:
+        kwargs = {"tags": {}}
+    num_resources = random.randint(min, max)
+    log.debug(
+        f"Adding {num_resources} {long_prefix} resources in {account.rtdname} {region.rtdname} with parents: {parents}, children: {children}"
+    )
+    for resource_num in range(num_resources):
+        resource_id_path = f"{id_path}/{short_prefix}{resource_num}"
+        log.debug(f"Adding {long_prefix} {resource_num} resource ({id_path})")
+        resource_id = short_prefix + get_id(resource_id_path)
+        resource = cls(resource_id, name=f"{long_prefix} {resource_num}", _account=account, _region=region, **kwargs)
+        graph.add_node(resource)
+        for parent in parents:
+            graph.add_edge(parent, resource)
+        child_parents = [resource]
+        if region != resource:
+            child_parents.append(region)
+        for child in children:
+            child(graph=graph, parents=child_parents, id_path=resource_id_path, account=account, region=region)
