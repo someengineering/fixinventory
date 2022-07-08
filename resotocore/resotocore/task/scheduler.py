d79d26801a58d9cc13de1abe43805cf00dd6df40
Lukas Lösche <lukas@some.engineering>
2022-07-08 15:30:09 +0200

    Use local ZoneInfo

--- resotocore/resotocore/task/scheduler.py
+++ resotocore/resotocore/task/scheduler.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from zoneinfo import ZoneInfo
 from typing import Callable, Any, List
 
 from apscheduler.executors.asyncio import AsyncIOExecutor
@@ -7,6 +6,8 @@ from apscheduler.job import Job
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
+
+from resotolib.utils import get_local_tzinfo
 
 
 class Scheduler:
@@ -18,7 +19,7 @@ class Scheduler:
             # max_instances: allowed parallel instances for one job
             # misfire_grace_time: seconds after the designated runtime that the job is still allowed to be run
             job_defaults={"coalesce": True, "max_instances": 32, "misfire_grace_time": 3600},
-            timezone=ZoneInfo("Etc/UTC"),
+            timezone=get_local_tzinfo(),
         )
 
     async def start(self) -> None:
