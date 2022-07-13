1e00be480a963c2118df2eacb89f8683e8097c1d
Matthias Veit <matthias_veit@yahoo.de>
2022-07-13 16:08:08 +0200

    fix merge

--- plugins/aws/test/collector_test.py
+++ plugins/aws/test/collector_test.py
@@ -22,6 +22,6 @@ def test_collect() -> None:
         return count
 
     assert len(ac.graph.edges) == 198
-    assert count_kind(AwsResource) == 82
+    assert count_kind(AwsResource) == 81
     for resource in all_resources:
         assert count_kind(resource) > 0, "No instances of {} found".format(resource.__name__)
