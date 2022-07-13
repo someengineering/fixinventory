e7da5a732cf4abb6108f2b92047f891373d8747e
Matthias Veit <matthias_veit@yahoo.de>
2022-07-13 17:55:13 +0200

    fix merge

--- plugins/aws/resoto_plugin_aws/collector.py
+++ plugins/aws/resoto_plugin_aws/collector.py
@@ -18,7 +18,12 @@ log = logging.getLogger("resoto.plugins.aws")
 
 global_resources: List[Type[AwsResource]] = iam.resources + route53.resources + ec2.global_resources + s3.resources
 regional_resources: List[Type[AwsResource]] = (
-    ec2.resources + elbv2.resources + autoscaling.resources + cloudwatch.resources + cloudformation.resources + eks.resources
+    ec2.resources
+    + elbv2.resources
+    + autoscaling.resources
+    + cloudwatch.resources
+    + cloudformation.resources
+    + eks.resources
 )
 all_resources: List[Type[AwsResource]] = global_resources + regional_resources
 
