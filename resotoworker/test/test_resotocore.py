a2edc986597c5e8588cb55fe74d9848a4f0a9566
Lukas Lösche <lukas@some.engineering>
2022-07-18 18:58:19 +0200

    Use proper graph

--- resotoworker/test/test_resotocore.py
+++ resotoworker/test/test_resotocore.py
@@ -1,10 +1,39 @@
 import json
+from argparse import ArgumentParser
 from typing import Dict, cast
 from resotolib.graph import Graph
 from resotoworker.resotocore import Resotocore
 import requests
 from test.fakeconfig import FakeConfig
 from resotolib.config import Config
+from resotolib.baseresources import BaseAccount, GraphRoot
+from resotolib.baseplugin import BaseCollectorPlugin
+from typing import ClassVar
+from attrs import define
+
+
+@define(eq=False)
+class ExampleAccount(BaseAccount):
+    kind: ClassVar[str] = "example_account"
+
+    def delete(self, graph: Graph) -> bool:
+        return NotImplemented
+
+
+class ExampleCollectorPlugin(BaseCollectorPlugin):
+    cloud = "example"
+
+    def collect(self) -> None:
+        account = ExampleAccount(id="Example Account")
+        self.graph.add_resource(self.graph.root, account)  # type: ignore
+
+    @staticmethod
+    def add_args(arg_parser: ArgumentParser) -> None:
+        pass
+
+    @staticmethod
+    def add_config(config: Config) -> None:
+        pass
 
 
 def test_resotocore() -> None:
@@ -36,7 +65,12 @@ def test_resotocore() -> None:
 
     core = Resotocore(make_query, config)
 
-    core.send_to_resotocore(Graph(), "task_123")
+    collector = ExampleCollectorPlugin()
+    collector.collect()
+    graph = Graph(root=GraphRoot(id="graph_root"))
+    graph.merge(collector.graph)
+
+    core.send_to_resotocore(graph, "task_123")
     print(recorded_headers)
 
     assert recorded_headers["Resoto-Worker-Task-Id"] == "task_123"
