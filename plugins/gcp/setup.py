8b6fb1117d8584b4d0c93678565fab78105c746a
Nikita Melkozerov <nikita@some.engineering>
2022-08-16 12:03:47 +0200

    [resoto] bump 2.4 release

--- plugins/gcp/setup.py
+++ plugins/gcp/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-gcp",
-    version="2.4.0a0",
+    version="2.4.0",
     description="Resoto GCP Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
