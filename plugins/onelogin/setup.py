b2958dfa7a06342fa10e6bd8ba81aa0c6764d89f
Nikita Melkozerov <nikita@some.engineering>
2022-08-17 12:01:01 +0200

    [resoto] bump main to 2.5 (#1075)
    * [resoto] bump main to 2.5
    
    * pyjwt fix

--- plugins/onelogin/setup.py
+++ plugins/onelogin/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-onelogin",
-    version="2.4.0a0",
+    version="2.5.0a0",
     description="Resoto OneLogin Plugin",
     license="Apache 2.0",
     packages=find_packages(),
