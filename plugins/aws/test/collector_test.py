dc1eb242396ef80d2137702ac2f289dffe4c461a
anjafr <anjafrhb@gmail.com>
2022-08-17 13:56:47 +0200

    update counters

--- plugins/aws/test/collector_test.py
+++ plugins/aws/test/collector_test.py
@@ -24,8 +24,8 @@ def test_collect() -> None:
                 count += 1
         return count
 
-    assert len(ac.graph.edges) == 303
-    assert count_kind(AwsResource) == 119
+    assert len(ac.graph.edges) == 305
+    assert count_kind(AwsResource) == 121
     for resource in all_resources:
         assert count_kind(resource) > 0, "No instances of {} found".format(resource.__name__)
 
