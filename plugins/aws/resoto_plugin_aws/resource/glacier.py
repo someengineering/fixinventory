8c036e7cfe1a1eb3211a6063d8b992707fb48f63
anjafr <anjafrhb@gmail.com>
2022-08-25 16:31:07 +0200

    try to collect vault jobs

--- plugins/aws/resoto_plugin_aws/resource/glacier.py
+++ plugins/aws/resoto_plugin_aws/resource/glacier.py
@@ -1,8 +1,137 @@
 from typing import ClassVar, Dict, List, Optional, Type, cast
 from attrs import define, field
 from resoto_plugin_aws.resource.base import AwsApiSpec, AwsResource, GraphBuilder
-from resotolib.json_bender import S, Bender
+from resotolib.baseresources import EdgeType
+from resotolib.json_bender import S, Bend, Bender, ForallBend
 from resotolib.types import Json
+
+
+@define(eq=False, slots=False)
+class AwsGlacierInventoryRetrievalParameters:
+    kind: ClassVar[str] = "aws_glacier_job_inventory_retrieval_parameters"
+    mapping: ClassVar[Dict[str, Bender]] = {
+        "output_format": S("Format"),
+        "start_date": S("StartDate"),
+        "end_date": S("EndDate"),
+        "limit": S("Limit"),
+    }
+    output_format: Optional[str] = field(default=None)
+    start_date: Optional[str] = field(default=None)
+    end_date: Optional[str] = field(default=None)
+    limit: Optional[str] = field(default=None)
+
+
+@define(eq=False, slots=False)
+class AwsGlacierSelectParameters:
+    kind: ClassVar[str] = "aws_glacier_job_select_parameters"
+    mapping: ClassVar[Dict[str, Bender]] = {
+        "input_serialization": S("InputSerialization"),
+        "expression_type": S("ExpressionType"),
+        "expression": S("Expression"),
+        "output_serialization": S("OutputSerialization"),
+    }
+    input_serialization: Optional[Dict[str, Dict[str, str]]] = field(default=None)
+    expression_type: Optional[str] = field(default=None)
+    expression: Optional[str] = field(default=None)
+    output_serialization: Optional[Dict[str, Dict[str, str]]] = field(default=None)
+
+
+@define(eq=False, slots=False)
+class AwsGlacierBucketEncryption:
+    kind: ClassVar[str] = "aws_glacier_bucket_encryption"
+    mapping: ClassVar[Dict[str, Bender]] = {
+        "encryption_type": S("EncryptionType"),
+        "kms_key_id": S("KMSKeyId"),
+        "kms_context": S("KMSContext"),
+    }
+    encryption_type: str = field(default=None)
+    kms_key_id: str = field(default=None)
+    kms_context: Optional[str] = field(default=None)
+
+
+@define(eq=False, slots=False)
+class AwsGlacierJobBucket:
+    kind: ClassVar[str] = "aws_glacier_job_bucket"
+    mapping: ClassVar[Dict[str, Bender]] = {
+        "bucket_name": S("BucketName"),
+        "prefix": S("Prefix"),
+        "encryption": S("Encryption") >> Bend(AwsGlacierBucketEncryption.mapping),
+        "canned_acl": S("CannedACL"),
+        "access_control_list": S("AccessControlList"),
+        "tagging": S("Tagging"),
+        "user_metadata": S("UserMetadata"),
+        "storage_class": S("StorageClass"),
+
+    }
+    bucket_name: Optional[str] = field(default=None)
+    prefix: Optional[str] = field(default=None)
+    encryption: Optional[AwsGlacierBucketEncryption] = field(default=None)
+    access_control_list: Optional[Dict[str, Dict[str, str]]] = field(default=None)
+    tagging: Optional[Dict[str, str]] = field(default=None)
+    user_metadata: Optional[Dict[str, str]] = field(default=None)
+    storage_class: Optional[str] = field(default=None)
+
+
+@define(eq=False, slots=False)
+class AwsGlacierJobOutputLocation:
+    kind: ClassVar[str] = "aws_glacier_job_output_location"
+    mapping: ClassVar[Dict[str, Bender]] = {
+        "s3": S("S3") >> Bend(AwsGlacierJobBucket.mapping),
+    }
+    s3: Optional[AwsGlacierJobBucket] = field(default=None)
+
+
+@define(eq=False, slots=False)
+class AwsGlacierJob(AwsResource):
+    kind: ClassVar[str] = "aws_glacier_job"
+    mapping: ClassVar[Dict[str, Bender]] = {
+        "id": S("JobId"),
+        "name": S("JobId"),
+        "ctime": S("CreationDate"),
+        # "arn": S("VaultARN"),
+        "description": S("JobDescription"),
+        "glacier_job_action": S("Action"),
+        "glacier_job_archive_id": S("ArchiveId"),
+        "glacier_job_vault_arn": S("VaultARN"),
+        "glacier_job_completed": S("Completed"),
+        "glacier_job_status_code": S("StatusCode"),
+        "glacier_job_status_message": S("StatusMessage"),
+        "glacier_job_archive_size_in_bytes": S("ArchiveSizeInBytes"),
+        "glacier_job_inventory_size_in_bytes": S("InventorySizeInBytes"),
+        "glacier_job_sns_topic": S("SNSTopic"),
+        "glacier_job_completion_date": S("CompletionDate"),
+        "glacier_job_sha256_tree_hash": S("SHA256TreeHash"),
+        "glacier_job_archive_sha256_tree_hash": S("ArchiveSHA256TreeHash"),
+        "glacier_job_retrieval_byte_range": S("RetrievalByteRange"),
+        "glacier_job_tier": S("Tier"),
+        "glacier_job_inventory_retrieval_parameters": S("InventoryRetrievalParameters") >> Bend(AwsGlacierInventoryRetrievalParameters.mapping),
+        "glacier_job_output_path": S("JobOutputPath"),
+        "glacier_job_select_parameters": S("SelectParameters") >> Bend(AwsGlacierSelectParameters.mapping),
+        "glacier_job_output_location": S("OutputLocation") >> Bend(AwsGlacierJobOutputLocation.mapping)
+
+    }
+    # TODO find out which are optional and which aren't
+    description: Optional[str] = field(default=None)
+    glacier_job_action: Optional[str] = field(default=None)
+    glacier_job_archive_id: Optional[str] = field(default=None)
+    glacier_job_vault_arn: Optional[str] = field(default=None)
+    glacier_job_completed: Optional[bool] = field(default=None)
+    glacier_job_status_code: Optional[str] = field(default=None)
+    glacier_job_status_message: Optional[str] = field(default=None)
+    glacier_job_archive_size_in_bytes: Optional[int] = field(default=None)
+    glacier_job_inventory_size_in_bytes: Optional[int] = field(default=None)
+    glacier_job_sns_topic: Optional[str] = field(default=None)
+    glacier_job_completion_date: Optional[str] = field(default=None)
+    glacier_job_sha256_tree_hash: Optional[str] = field(default=None)
+    glacier_job_archive_sha256_tree_hash: Optional[str] = field(default=None)
+    glacier_job_retrieval_byte_range: Optional[str] = field(default=None)
+    glacier_job_tier: Optional[str] = field(default=None)
+    glacier_job_inventory_retrieval_parameters: Optional[AwsGlacierInventoryRetrievalParameters] = field(default=None)
+    glacier_job_output_path: Optional[str] = field(default=None)
+    glacier_job_select_parameters: Optional[AwsGlacierSelectParameters] = field(default=None)
+    glacier_job_output_location: Optional[AwsGlacierJobOutputLocation] = field(default=None)
+
+
 
 @define(eq=False, slots=False)
 class AwsGlacierVault(AwsResource):
@@ -28,9 +157,15 @@ class AwsGlacierVault(AwsResource):
             if tags:
                 vault.tags = cast(Dict[str, Optional[str]], tags)
 
-        for js in json:
-            vault = cls.from_api(js)
-            builder.add_node(vault, js)
-            builder.submit_work_shared_pool(add_tags, vault)
+        for vault in json:
+            vault_instance = cls.from_api(vault)
+            builder.add_node(vault_instance, vault)
+            builder.submit_work_shared_pool(add_tags, vault_instance)
+            for job in builder.client.list(
+                "glacier", "list-jobs", "JobList", vaultName=vault_instance.name
+            ):
+                job_instance = AwsGlacierJob.from_api(job)
+                builder.add_node(job_instance, job)
+                builder.add_edge(vault_instance, EdgeType.default, node=job_instance)
 
-resources: List[AwsResource] = [AwsGlacierVault]
+resources: List[AwsResource] = [AwsGlacierVault, AwsGlacierJob]
--- plugins/aws/test/resources/glacier_test.py
+++ plugins/aws/test/resources/glacier_test.py
@@ -1,6 +1,7 @@
-from resoto_plugin_aws.resource.glacier import AwsGlacierVault
+from collections import defaultdict
+from resoto_plugin_aws.resource.glacier import AwsGlacierVault, AwsGlacierJob
 from test.resources import round_trip_for
-from typing import Any, cast
+from typing import Any, Dict, cast
 from types import SimpleNamespace
 from resoto_plugin_aws.aws_client import AwsClient
 
@@ -9,3 +10,7 @@ def test_vaults() -> None:
     vault, builder = round_trip_for(AwsGlacierVault)
     assert len(builder.resources_of(AwsGlacierVault)) == 1
     assert len(vault.tags) == 1
+    type_count: Dict[str, int] = defaultdict(int)
+    for node in builder.graph.nodes:
+        type_count[node.kind] += 1
+    assert type_count["aws_glacier_job"] == 1
