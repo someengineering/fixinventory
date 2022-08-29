8e23a02532d1c759a19af7ee65b8be5c7d7eb63f
anjafr <anjafrhb@gmail.com>
2022-08-29 20:08:04 +0200

    add called apis

--- plugins/aws/resoto_plugin_aws/resource/glacier.py
+++ plugins/aws/resoto_plugin_aws/resource/glacier.py
@@ -181,6 +181,14 @@ class AwsGlacierVault(AwsResource):
     glacier_size_in_bytes: Optional[int] = field(default=None)
 
     @classmethod
+    def called_apis(cls) -> List[AwsApiSpec]:
+        return [
+            cls.api_spec,
+            AwsApiSpec(cls.api_spec.service, "list-tags-for-vault"),
+            AwsApiSpec(cls.api_spec.service, "list-jobs"),
+        ]
+
+    @classmethod
     def collect(cls: Type[AwsResource], json: List[Json], builder: GraphBuilder) -> None:
         def add_tags(vault: AwsGlacierVault) -> None:
             tags = builder.client.list("glacier", "list-tags-for-vault", "Tags", vaultName=vault.name)
