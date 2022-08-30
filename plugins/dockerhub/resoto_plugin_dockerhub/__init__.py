5d6c0f57f03c2c0660f1c39a88512e9f3e42dcc2
Lukas Lösche <lukas@some.engineering>
2022-08-30 19:41:42 +0200

    [plugins/dockerhub][feat] Docker Hub MVP collector (#1137)
    * [plugins/dockerhub][feat] Docker Hub MVP collector

---
+++ plugins/dockerhub/resoto_plugin_dockerhub/__init__.py
@@ -0,0 +1,38 @@
+import requests
+from resotolib.logger import log
+from resotolib.baseplugin import BaseCollectorPlugin
+from resotolib.config import Config
+from .config import DockerHubConfig
+from .resources import (
+    DockerHubNamespace,
+    DockerHubRepository,
+)
+
+
+class DockerHubCollectorPlugin(BaseCollectorPlugin):
+    cloud = "dockerhub"
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.dockerhub_uri = "https://hub.docker.com/v2/repositories/"
+
+    def collect(self) -> None:
+        log.debug("plugin: collecting Docker Hub resources")
+
+        for namespace in Config.dockerhub.namespaces:
+            log.debug(f"Collecting Docker Hub resources in namespace {namespace}")
+            r = requests.get(f"{self.dockerhub_uri}{namespace}")
+            if r.status_code != 200:
+                log.error(f"Error collecting Docker Hub resources in namespace {namespace}: {r.status_code} {r.text}")
+                continue
+            count = r.json()["count"]
+            results = r.json()["results"]
+            ns = DockerHubNamespace(id=namespace, count=count)
+            self.graph.add_resource(self.graph.root, ns)
+            for repo in results:
+                r = DockerHubRepository.new(repo)
+                self.graph.add_resource(ns, r)
+
+    @staticmethod
+    def add_config(config: Config) -> None:
+        config.add_config(DockerHubConfig)
