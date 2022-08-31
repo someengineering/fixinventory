619d7b008c684b99fd1943d9c0b94d2cd50aec30
Lukas Lösche <lukas@some.engineering>
2022-08-31 23:40:03 +0200

    Add pagination support

--- plugins/dockerhub/resoto_plugin_dockerhub/__init__.py
+++ plugins/dockerhub/resoto_plugin_dockerhub/__init__.py
@@ -3,6 +3,7 @@ from resotolib.logger import log
 from resotolib.baseplugin import BaseCollectorPlugin
 from resotolib.config import Config
 from .config import DockerHubConfig
+from typing import Optional
 from .resources import (
     DockerHubNamespace,
     DockerHubRepository,
@@ -21,18 +22,34 @@ class DockerHubCollectorPlugin(BaseCollectorPlugin):
 
         for namespace in Config.dockerhub.namespaces:
             log.debug(f"Collecting Docker Hub resources in namespace {namespace}")
-            r = requests.get(f"{self.dockerhub_uri}{namespace}")
-            if r.status_code != 200:
-                log.error(f"Error collecting Docker Hub resources in namespace {namespace}: {r.status_code} {r.text}")
-                continue
-            count = r.json()["count"]
-            results = r.json()["results"]
-            ns = DockerHubNamespace(id=namespace, count=count)
-            self.graph.add_resource(self.graph.root, ns)
-            for repo in results:
-                r = DockerHubRepository.new(repo)
-                self.graph.add_resource(ns, r)
+
+            next = f"{self.dockerhub_uri}{namespace}"
+            ns = None
+            while next is not None:
+                r = fetch_uri(next)
+                if r is None:
+                    continue
+                count = r.get("count")
+                results = r.get("results", [])
+                next = r.get("next")
+
+                if ns is None:
+                    ns = DockerHubNamespace(id=namespace, count=count)
+                    self.graph.add_resource(self.graph.root, ns)
+
+                for repo in results:
+                    r = DockerHubRepository.new(repo)
+                    self.graph.add_resource(ns, r)
 
     @staticmethod
     def add_config(config: Config) -> None:
         config.add_config(DockerHubConfig)
+
+
+def fetch_uri(uri: str) -> Optional[dict]:
+    log.debug(f"Getting {uri}")
+    r = requests.get(uri)
+    if r.status_code != 200:
+        log.error(f"Error collecting Docker Hub resources {uri}: {r.status_code} {r.text}")
+        return None
+    return r.json()
--- plugins/dockerhub/resoto_plugin_dockerhub/resources.py
+++ plugins/dockerhub/resoto_plugin_dockerhub/resources.py
@@ -7,7 +7,6 @@ from resotolib.baseresources import (
     BaseResource,
 )
 
-
 @define(eq=False, slots=False)
 class DockerHubResource:
     kind: ClassVar[str] = "dockerhub_resource"
@@ -43,6 +42,11 @@ class DockerHubRepository(DockerHubResource, BaseResource):
 
     @staticmethod
     def new(data: Dict) -> BaseResource:
+        # Docker Hub API returns [None] for media types
+        # This removes all None values from the list
+        media_types = list(filter((None).__ne__, data.get("media_types", [])))
+        if len(media_types) == 0:
+            media_types = None
         return DockerHubRepository(
             id=data.get("name"),
             repository_type=data.get("repository_type"),
@@ -52,7 +56,7 @@ class DockerHubRepository(DockerHubResource, BaseResource):
             mtime=convert_date(data.get("last_updated")),
             ctime=convert_date(data.get("date_registered")),
             affiliation=data.get("affiliation"),
-            media_types=data.get("media_types"),
+            media_types=media_types,
         )
 
 
