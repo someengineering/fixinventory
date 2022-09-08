cca83748ecb0d2b6f63a32cb356d82f0198f1046
anjafr <anjafrhb@gmail.com>
2022-09-08 10:45:12 +0200

    update counters

--- plugins/aws/test/collector_test.py
+++ plugins/aws/test/collector_test.py
@@ -34,8 +34,8 @@ def test_collect(account_collector: AwsAccountCollector) -> None:
     # make sure all threads have been joined
     assert len(threading.enumerate()) == 1
     # ensure the correct number of nodes and edges
-    assert count_kind(AwsResource) == 135
-    assert len(account_collector.graph.edges) == 344
+    assert count_kind(AwsResource) == 138
+    assert len(account_collector.graph.edges) == 349
 
 
 def test_dependencies() -> None:
