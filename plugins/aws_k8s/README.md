a05dfaf25c25aa0c7af051e818db276c36b97dfe
Nikita Melkozerov <nikita@some.engineering>
2022-09-09 12:19:25 +0200

    [plugins/aws_k8s][feat] Support aws_k8s deferred edge collection (#1156)

---
+++ plugins/aws_k8s/README.md
@@ -0,0 +1,3 @@
+# resoto-plugin-aws-k8s
+AWS - K8s Collector Plugin for Resoto: link together your AWS and k8s cluster resources.
+
---
+++ plugins/aws_k8s/setup.py
@@ -0,0 +1,46 @@
+import os
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="resoto-plugin-aws-k8s",
+    version="3.0.0a0",
+    description="Resoto AWS-K8s Collector Plugin",
+    license="Apache 2.0",
+    packages=find_packages(),
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    entry_points={"resoto.plugins": ["aws_k8s_collector = resoto_plugin_aws_k8s:AWSK8sCollectorPlugin"]},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requirements,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    classifiers=[
+        # Current project status
+        "Development Status :: 4 - Beta",
+        # Audience
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        # License information
+        "License :: OSI Approved :: Apache Software License",
+        # Supported python versions
+        "Programming Language :: Python :: 3.9",
+        # Supported OS's
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        # Extra metadata
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Topic :: Security",
+        "Topic :: Utilities",
+    ],
+    keywords="cloud security",
+)
