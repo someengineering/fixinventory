30c29b0df0abf9e8b9ab5ac78f0fef38f9d34d1b
Nikita Melkozerov <nikita@some.engineering>
2022-09-19 12:16:37 +0200

    [plugins/aws][fix] Do not collect the default AwsDataCatalog (#1181)

--- plugins/aws/resoto_plugin_aws/resource/athena.py
+++ plugins/aws/resoto_plugin_aws/resource/athena.py
@@ -210,7 +210,8 @@ class AwsAthenaDataCatalog(AwsResource):
                 data_catalog.tags = bend(S("Tags", default=[]) >> ToDict(), tags[0])
 
         for js in json:
-            if (name := js.get("CatalogName")) is not None and isinstance(name, str):
+            # we filter out the default data catalog as it is not possible to do much with it
+            if (name := js.get("CatalogName")) is not None and isinstance(name, str) and name != "AwsDataCatalog":
                 catalog = fetch_data_catalog(name)
                 if catalog is not None:
                     builder.add_node(catalog)
