6ce8ca8673609dc6ae9eb44f8e01997815edcf28
Nikita Melkozerov <nikita@some.engineering>
2022-09-23 16:21:31 +0200

    [plugins/digitalocean][fix] Fix the snapshots regions collection (#1194)

--- plugins/digitalocean/test/fixtures/snapshots.py
+++ plugins/digitalocean/test/fixtures/snapshots.py
@@ -2,7 +2,7 @@ snapshots = [
     {
         "id": "103198134",
         "name": "ubuntu-s-1vcpu-1gb-fra1-01-1646325828761",
-        "regions": ["fra1"],
+        "regions": ["fra1", "ams3"],
         "created_at": "2022-03-03T16:43:51Z",
         "resource_id": "289110074",
         "resource_type": "droplet",
