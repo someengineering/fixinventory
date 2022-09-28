3b46e1ee3eb8b034758374cab1fa489b59ea5abc
azagaya <azagaya.games@gmail.com>
2022-09-28 11:05:08 +0100

    Set default config value

--- plugins/scarf/resoto_plugin_scarf/config.py
+++ plugins/scarf/resoto_plugin_scarf/config.py
@@ -5,7 +5,10 @@ from typing import List, ClassVar
 @define
 class ScarfConfig:
     kind: ClassVar[str] = "scarf"
-    token: str
+    token: str = field(
+        default="",
+        metadata={"description": "Scarf API token."}
+    )
     organizations: List[str] = field(
         factory=list,
         metadata={"description": "Scarf organizations to collect."},
