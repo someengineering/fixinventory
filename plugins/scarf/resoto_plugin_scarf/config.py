21d749b7a83e7440758cc05a47ed68346ed063b7
Lukas Lösche <lukas@some.engineering>
2022-09-28 11:32:09 +0100

    search for multiple org criteria

--- plugins/scarf/resoto_plugin_scarf/config.py
+++ plugins/scarf/resoto_plugin_scarf/config.py
@@ -5,10 +5,7 @@ from typing import List, ClassVar
 @define
 class ScarfConfig:
     kind: ClassVar[str] = "scarf"
-    token: str = field(
-        default="",
-        metadata={"description": "Scarf API token."}
-    )
+    token: str = field(default="", metadata={"description": "Scarf API token."})
     organizations: List[str] = field(
         factory=list,
         metadata={"description": "Scarf organizations to collect."},
