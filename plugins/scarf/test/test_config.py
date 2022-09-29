966a36cda6aab72b355a7b8b04598bf321f721dd
Lukas Lösche <lukas@some.engineering>
2022-09-29 13:16:11 +0100

    Update test

--- plugins/scarf/test/test_config.py
+++ plugins/scarf/test/test_config.py
@@ -1,9 +1,12 @@
 from resotolib.config import Config
-from resoto_plugin_dockerhub import DockerHubCollectorPlugin
+from resoto_plugin_scarf import ScarfCollectorPlugin
 
 
 def test_config():
     config = Config("dummy", "dummy")
-    DockerHubCollectorPlugin.add_config(config)
+    ScarfCollectorPlugin.add_config(config)
     config.init_default_config()
-    assert Config.dockerhub.namespaces == []
+    assert Config.scarf.email == ""
+    assert Config.scarf.password == ""
+    assert Config.scarf.organizations == []
+
