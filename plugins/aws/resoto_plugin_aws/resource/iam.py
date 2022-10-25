8be2e2193987f48129c190a7eb2ae1066c920f13
anjafr <anjafrhb@gmail.com>
2022-10-25 16:35:45 +0200

    use list-users for PasswordLastUsed

--- plugins/aws/resoto_plugin_aws/resource/iam.py
+++ plugins/aws/resoto_plugin_aws/resource/iam.py
@@ -391,11 +391,14 @@ class AwsIamUser(AwsResource, BaseUser):
             cls.api_spec,
             AwsApiSpec("iam", "list-access-keys"),
             AwsApiSpec("iam", "get-access-key-last-used"),
-            AwsApiSpec("iam", "get-user"),
+            AwsApiSpec("iam", "list-users"),
         ]
 
     @classmethod
     def collect(cls: Type[AwsResource], json_list: List[Json], builder: GraphBuilder) -> None:
+        name_password_last_used_map: Dict[str, str] = {"UserName": "PasswordLastUsed"}
+        for user in builder.client.list("iam", "list-users", "Users"):
+            name_password_last_used_map[user["UserName"]] = user["PasswordLastUsed"]
         for json in json_list:
             for js in json.get("GroupDetailList", []):
                 builder.add_node(AwsIamGroup.from_api(js), js)
@@ -407,9 +410,7 @@ class AwsIamUser(AwsResource, BaseUser):
                 builder.add_node(AwsIamPolicy.from_api(js), js)
 
             for js in json.get("UserDetailList", []):
-                user_details = builder.client.get("iam", "get-user", "User", UserName=js["UserName"])
-                if user_details:
-                    js["PasswordLastUsed"] = user_details["PasswordLastUsed"]
+                js["PasswordLastUsed"] = name_password_last_used_map[js["UserName"]]
                 user = AwsIamUser.from_api(js)
                 builder.add_node(user, js)
                 # add all iam access keys for this user
