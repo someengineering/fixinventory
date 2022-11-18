422f4820e8a3016e85e3a42f952643304cd79a99
anjafr <anjafrhb@gmail.com>
2022-11-18 18:32:12 +0100

    add deferred edges k8s <-> aws

--- plugins/aws_k8s/resoto_plugin_aws_k8s/__init__.py
+++ plugins/aws_k8s/resoto_plugin_aws_k8s/__init__.py
@@ -31,6 +31,37 @@ def link_k8s_cluster_to_eks_cluster(graph: Graph, resource: BaseResource) -> Non
         )
 
 
+def link_node_to_ec2_instance(graph: Graph, resource: BaseResource) -> None:
+    if resource.kind == "kubernetes_node":
+        if (pid := rgetattr(resource, "node_spec.provider_id", None)) and (pid.startswith("aws://")):
+            _, ec2_zone_and_instance_id = pid.split("aws://")
+            ec2_instance_id = ec2_zone_and_instance_id.split("/")[2]
+            graph.add_deferred_edge(
+                BySearchCriteria(f"is(aws_ec2_instance) and reported.id={ec2_instance_id}"),
+                ByNodeId(resource.chksum),
+            )
+
+
+def link_service_to_elb(graph: Graph, resource: BaseResource) -> None:
+    if resource.kind == "kubernetes_service":
+        if (rgetattr(resource, "service_spec.type", None) == "LoadBalancer") and (
+            ingresses := rgetattr(resource, "service_status.load_balancer.ingress", None)
+        ):
+            for ingress in ingresses:
+                if elb_hostname := rgetattr(ingress, "hostname", None):
+                    graph.add_deferred_edge(
+                        BySearchCriteria(f"is(aws_elb) and reported.id={elb_hostname}"), ByNodeId(resource.chksum)
+                    )
+
+
+def link_pv_to_ebs_volume(graph: Graph, resource: BaseResource) -> None:
+    if resource.kind == "kubernetes_persistent_volume":
+        if vol_id := rgetattr(resource, "persistent_volume_spec.aws_elastic_block_store.volume_id", None):
+            graph.add_deferred_edge(
+                BySearchCriteria(f"is(aws_ec2_volume) and reported.id={vol_id}"), ByNodeId(resource.chksum)
+            )
+
+
 class AWSK8sCollectorPlugin(BasePostCollectPlugin):
     name = "aws_k8s"
     activate_with: Set[str] = {"aws", "k8s"}
@@ -41,5 +72,8 @@ class AWSK8sCollectorPlugin(BasePostCollectPlugin):
             if isinstance(node, BaseResource):
                 link_k8s_node_to_aws_nodegroup(graph, node)
                 link_k8s_cluster_to_eks_cluster(graph, node)
+                link_pv_to_ebs_volume(graph, node)
+                link_service_to_elb(graph, node)
+                link_node_to_ec2_instance(graph, node)
             else:
                 log.warn(f"Node {node} is not a BaseResource")
