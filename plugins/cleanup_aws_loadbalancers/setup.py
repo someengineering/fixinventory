0f954d3a2d16a32a51c348ef9a0621e277a55ee8
Lukas Lösche <lukas@some.engineering>
2022-11-21 23:32:23 +0100

    [resoto][chore] Bump 3.0.0a2 (#1295)

--- plugins/cleanup_aws_loadbalancers/setup.py
+++ plugins/cleanup_aws_loadbalancers/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="resoto-plugin-cleanup-aws-loadbalancers",
-    version="3.0.0a1",
+    version="3.0.0a2",
     description="AWS Loadbalancers Cleaner Plugin",
     license="Apache 2.0",
     packages=find_packages(),
