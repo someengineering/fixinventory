1f5bdbc08c3af8d84a5c7e5fc1f4011931961685
anjafr <anjafrhb@gmail.com>
2022-11-21 15:34:48 +0100

    clean up

--- plugins/aws_k8s/test/test_collector.py
+++ plugins/aws_k8s/test/test_collector.py
@@ -5,29 +5,6 @@ from typing import ClassVar
 from types import SimpleNamespace
 
 
-# class KubernetesService(BaseResource):
-#     kind: ClassVar[str] = "kubernetes_service"
-#     id = "123"
-#     service_spec = SimpleNamespace(type = "LoadBalancer")
-#     service_status = SimpleNamespace(load_balancer = SimpleNamespace(ingress = [{"hostname": "miau"}]))
-#     # labels = {"eks.amazonaws.com/nodegroup": "main"}
-
-#     def delete(self, graph: Graph) -> bool:
-#         return False
-
-
-# def test_post_collect() -> None:
-
-#     plugin = AWSK8sCollectorPlugin()
-
-#     node = KubernetesService(id="123")
-
-#     graph = Graph(root=node)
-#     plugin.post_collect(graph)
-
-#     assert len(graph.deferred_edges) == 1
-
-
 class KubernetesNode(BaseResource):
     kind: ClassVar[str] = "kubernetes_node"
     node_spec = SimpleNamespace(provider_id="aws:///eu-central-1a/123")
