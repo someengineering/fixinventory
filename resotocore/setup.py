7c197dd1d70efc89ff9986db5be25b09b2a7b227
Lukas Lösche <lukas@some.engineering>
2022-11-28 23:06:03 +0100

    [resoto][chore] Bump 3.0.0rc3

--- resotocore/setup.py
+++ resotocore/setup.py
@@ -41,7 +41,7 @@ class PostDevelopCommand(develop):
 
 setup(
     name="resotocore",
-    version="3.0.0rc2",
+    version="3.0.0rc3",
     description="Keeps all the things.",
     python_requires=">=3.5",
     classifiers=["Programming Language :: Python :: 3"],
