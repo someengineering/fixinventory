4a0d420106847737fc2f74dba3658c286ad655b8
anjafr <anjafrhb@gmail.com>
2022-12-06 15:19:13 +0100

    field level encryption responses

--- plugins/aws/tools/response_syntax_to_json.py
+++ plugins/aws/tools/response_syntax_to_json.py
@@ -1,47 +1,49 @@
 import json
-from datetime import datetime
+from datetime import date, datetime
+from resotolib.utils import utc_str
+
+# subclass JSONEncoder
+class DateTimeEncoder(json.JSONEncoder):
+        #Override the default method
+        def default(self, obj):
+            if isinstance(obj, (date, datetime)):
+                return utc_str(obj)
+
 
 #1. paste the boto3 Response Syntax example here
-#2. find and replace '|' with a space
+#2. find and replace "'|'"" with a space or something
 #3. run
+
 response={
-    'ContinuousDeploymentPolicyList': {
+    'FieldLevelEncryptionProfileList': {
         'NextMarker': 'string',
         'MaxItems': 123,
         'Quantity': 123,
         'Items': [
             {
-                'ContinuousDeploymentPolicy': {
-                    'Id': 'string',
-                    'LastModifiedTime': datetime(2015, 1, 1),
-                    'ContinuousDeploymentPolicyConfig': {
-                        'StagingDistributionDnsNames': {
-                            'Quantity': 123,
-                            'Items': [
-                                'string',
-                            ]
+                'Id': 'string',
+                'LastModifiedTime': datetime(2015, 1, 1),
+                'Name': 'string',
+                'EncryptionEntities': {
+                    'Quantity': 123,
+                    'Items': [
+                        {
+                            'PublicKeyId': 'string',
+                            'ProviderId': 'string',
+                            'FieldPatterns': {
+                                'Quantity': 123,
+                                'Items': [
+                                    'string',
+                                ]
+                            }
                         },
-                        'Enabled': True|False,
-                        'TrafficConfig': {
-                            'SingleWeightConfig': {
-                                'Weight': 0.5,
-                                'SessionStickinessConfig': {
-                                    'IdleTTL': 123,
-                                    'MaximumTTL': 123
-                                }
-                            },
-                            'SingleHeaderConfig': {
-                                'Header': 'string',
-                                'Value': 'string'
-                            },
-                            'Type': 'SingleWeight SingleHeader'
-                        }
-                    }
-                }
+                    ]
+                },
+                'Comment': 'string'
             },
         ]
     }
 }
 
 
-print (json.dumps(response, indent=4, default=str))
+print (json.dumps(response, indent=4, cls=DateTimeEncoder))
