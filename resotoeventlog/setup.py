de2fba1b0a265d58237399bade4246f0a5de6d73
Matthias Veit <matthias_veit@yahoo.de>
2022-12-27 15:36:54 +0100

    [resoto][chore] Bump next version

--- resotoeventlog/setup.py
+++ resotoeventlog/setup.py
@@ -20,7 +20,7 @@ setup_requirements = ["pytest-runner"]
 
 setup(
     name="resotoeventlog",
-    version="3.0.3",
+    version="3.0.4",
     description="Event log aggregator for resoto.",
     python_requires=">=3.5",
     classifiers=["Programming Language :: Python :: 3"],
