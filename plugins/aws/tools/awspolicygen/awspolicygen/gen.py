6536a95d19d49c10ccbb43aa9e4446d981197a01
Lukas Lösche <lukas@some.engineering>
2023-02-03 17:49:05 +0100

    [fix][plugins/aws] Fix inline policy length (#1414)

--- plugins/aws/tools/awspolicygen/awspolicygen/gen.py
+++ plugins/aws/tools/awspolicygen/awspolicygen/gen.py
@@ -23,7 +23,7 @@ org_list_policy = {
 }
 
 
-def get_policies() -> list:
+def get_policies(org_list: bool = True, collect: bool = True, mutate: bool = True) -> list:
     def iam_statement(name: str, apis: list[AwsApiSpec]) -> tuple[set[str], str]:
         permissions = {api.iam_permission() for api in apis}
         statement = {
@@ -35,14 +35,19 @@ def get_policies() -> list:
         }
         return statement
 
-    collect_policy = iam_statement("ResotoCollect", called_collect_apis())
-    mutate_policy = iam_statement("ResotoMutate", called_mutator_apis())
-    policies = [org_list_policy, collect_policy, mutate_policy]
+    policies = []
+    if org_list:
+        policies.append(org_list_policy)
+    if collect:
+        collect_policy = iam_statement("ResotoCollect", called_collect_apis())
+        policies.append(collect_policy)
+    if mutate:
+        mutate_policy = iam_statement("ResotoMutate", called_mutator_apis())
+        policies.append(mutate_policy)
     return policies
 
 
 def get_cf_template() -> str:
-    policies = get_policies()
     local_path = os.path.abspath(os.path.dirname(__file__))
     template_path = os.path.join(local_path, "templates/resoto-role.template.in")
     with open(template_path, "r") as f:
@@ -53,7 +58,7 @@ def get_cf_template() -> str:
 
     indent_by = len(template[-1]) - len(template[-1].lstrip())
 
-    policies = safe_dump(get_policies(), sort_keys=False)
+    policies = safe_dump(get_policies(collect=False), sort_keys=False)
     policies = "\n".join(" " * indent_by + line for line in policies.splitlines())
 
-    return "".join(template) + policies
+    return "".join(template) + policies + "\n"
