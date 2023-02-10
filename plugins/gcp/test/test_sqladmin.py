ca01e23f75f03f970d46557c7899037b6177d9aa
anjafr <anjafrhb@gmail.com>
2023-02-10 16:17:25 +0100

    edges for database resources

--- plugins/gcp/test/test_sqladmin.py
+++ plugins/gcp/test/test_sqladmin.py
@@ -1,4 +1,4 @@
-from .random_client import roundtrip
+from .random_client import connect_resource, roundtrip
 from resoto_plugin_gcp.resources.base import GraphBuilder
 from resoto_plugin_gcp.resources.sqladmin import *
 
@@ -8,8 +8,12 @@ def test_gcp_sql_flag(random_builder: GraphBuilder) -> None:
 
 
 def test_gcp_sql_database_instance(random_builder: GraphBuilder) -> None:
-    roundtrip(GcpSqlDatabaseInstance, random_builder)
+    db = roundtrip(GcpSqlDatabaseInstance, random_builder)
+    connect_resource(random_builder, db, GcpSslCertificate, selfLink=db.instance_server_ca_cert.self_link)
+    assert len(random_builder.edges_of(GcpSslCertificate, GcpSqlDatabaseInstance)) == 1
 
 
 def test_gcp_sql_operation(random_builder: GraphBuilder) -> None:
-    roundtrip(GcpSqlOperation, random_builder)
+    op = roundtrip(GcpSqlOperation, random_builder)
+    connect_resource(random_builder, op, GcpSqlDatabaseInstance, name=op.operation_target_id)
+    assert len(random_builder.edges_of(GcpSqlDatabaseInstance, GcpSqlOperation)) == 1
