5af98276a7f65cccc2771d5c397364d9f2f09bd2
Matthias Veit <matthias_veit@yahoo.de>
2023-02-13 11:08:15 +0100

    remove the test

--- resotolib/test/test_graph.py
+++ resotolib/test/test_graph.py
@@ -1,7 +1,7 @@
 from platform import python_implementation
 
 import pytest
-from resotolib.graph import Graph, GraphContainer, GraphExportIterator
+from resotolib.graph import Graph, GraphExportIterator
 from resotolib.baseresources import BaseResource, EdgeType, GraphRoot
 import resotolib.logger as logger
 from attrs import define
@@ -27,21 +27,6 @@ def test_graph():
     g.add_edge(n1, n2)
     assert len(g.nodes) == 2
     assert len(g.edges) == 1
-
-
-def test_graph_container():
-    gc = GraphContainer(cache_graph=False)
-    g = Graph()
-    n1 = SomeTestResource(id="foo", tags={})
-    n2 = SomeTestResource(id="bar", tags={})
-    g.add_node(n1)
-    gc.graph.add_resource(gc.GRAPH_ROOT, n2)
-    gc.add(g)
-    gc.graph.add_edge(n1, n2)
-    assert len(gc.graph.nodes) == 3
-    assert len(gc.graph.edges) == 2
-    assert gc.graph.search_first("id", "bar") == n2
-    assert gc.graph.search_first_parent_class(n2, SomeTestResource) == n1
 
 
 def test_graph_merge():
