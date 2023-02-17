9d5bcb55331d9c2d254edf0f6605ac06378eadaf
Matthias Veit <matthias_veit@yahoo.de>
2023-02-17 12:51:22 +0100

    add test for dash underscore handling

--- resotocore/tests/resotocore/cli/command_test.py
+++ resotocore/tests/resotocore/cli/command_test.py
@@ -883,7 +883,7 @@ async def test_jira_alias(cli: CLI, echo_http_server: Tuple[int, List[Tuple[Requ
 async def test_pagerduty_alias(cli: CLI, echo_http_server: Tuple[int, List[Tuple[Request, Json]]]) -> None:
     port, requests = echo_http_server
     result = await cli.execute_cli_command(
-        f'search is(bla) | pagerduty --webhook_url "http://localhost:{port}/success" --summary test --routing_key 123 --dedup_key 234',
+        f'search is(bla) | pagerduty --webhook-url "http://localhost:{port}/success" --summary test --routing-key 123 --dedup-key 234',
         stream.list,
     )
     assert result == [["1 requests with status 200 sent."]]
