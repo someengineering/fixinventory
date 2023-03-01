8bee0cbf47e40eda9d8a7edb6113c35a0055297e
Lukas Lösche <lukas@some.engineering>
2023-03-01 17:15:24 +0100

    [resoto][chore] Bump 3.2.5

--- plugins/dockerhub/setup.py
+++ plugins/dockerhub/setup.py
@@ -10,7 +10,7 @@ def read(file_name: str) -> str:
 
 setup(
     name="resoto-plugin-dockerhub",
-    version="3.2.4",
+    version="3.2.5",
     description="Resoto Docker Hub Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
