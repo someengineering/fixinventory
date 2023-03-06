77d3a8fa02a788157777d6cf3bef9323e5cfe365
anjafr <anjafrhb@gmail.com>
2023-03-06 17:22:21 +0100

    don't split twice

--- plugins/gcp/resoto_plugin_gcp/resources/compute.py
+++ plugins/gcp/resoto_plugin_gcp/resources/compute.py
@@ -2754,8 +2754,8 @@ class GcpInstance(GcpResource, BaseInstance):
             set([instance.instance_machine_type for instance in result if "custom" in instance.instance_machine_type])
         )
         for machine_type in custom_machine_types:
-            zone = machine_type.split("/")[-3]
-            name = machine_type.split("/")[-1]
+            # example: https://www.googleapis.com/compute/v1/projects/vpc-host-nonprod-320811/zones/us-east1-b/machineTypes/e2-custom-medium-1024
+            zone, _, name = machine_type.split("/")[-3:]
             GcpMachineType.collect_individual(builder, zone, name)
 
         return result
