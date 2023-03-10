0bad9931c7a8d7218b5078bae62ed157415aba46
Lukas Lösche <lukas@some.engineering>
2023-03-10 23:18:40 +0100

    [resoto][chore] Bump 3.2.7 (#1499)
    * [resoto][chore] Bump 3.2.7

--- plugins/cleanup_untagged/setup.py
+++ plugins/cleanup_untagged/setup.py
@@ -10,7 +10,7 @@ def read(file_name: str) -> str:
 
 setup(
     name="resoto-plugin-cleanup-untagged",
-    version="3.2.6",
+    version="3.2.7",
     description="Resoto Cleanup Untagged Plugin",
     license="Apache 2.0",
     packages=find_packages(),
