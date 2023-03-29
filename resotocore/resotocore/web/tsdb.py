d81b35ab30d167d26614fa87bde14be5f3c4149e
Matthias Veit <matthias_veit@yahoo.de>
2023-03-29 21:15:46 +0200

    [resotocore][fix] Strip request specific headers

--- resotocore/resotocore/web/tsdb.py
+++ resotocore/resotocore/web/tsdb.py
@@ -6,6 +6,7 @@ from typing import Callable, Awaitable
 from aiohttp import ClientConnectionError
 from aiohttp.web import HTTPNotFound, Request, StreamResponse
 from aiohttp.web_exceptions import HTTPBadGateway
+from multidict import CIMultiDict
 
 from resotocore.web import api  # pylint: disable=unused-import # prevent circular import
 from resotocore.web.directives import enable_compression
@@ -23,12 +24,19 @@ def hostname() -> str:
 
 # Proxy request to configured tsdb endpoint
 def tsdb(api_handler: "api.Api") -> Callable[[Request], Awaitable[StreamResponse]]:
+    def drop_request_specific_headers(headers: CIMultiDict[str]) -> None:
+        # since we stream the content (chunked), we are not allowed to set the content length.
+        headers.popall("Content-Length", None)
+        headers.popall("Content-Encoding", None)
+        headers.popall("Connection", None)
+        headers.popall("Transfer-Encoding", None)
+        headers.popall("Accept-Encoding", None)
+        headers.popall("Host", None)
+
     async def proxy_request(request: Request) -> StreamResponse:
         if api_handler.config.api.tsdb_proxy_url:
             in_headers = request.headers.copy()
-            # since we stream the content (chunked), we are not allowed to set the content length.
-            in_headers.popall("Content-Length", "none")
-            in_headers.popall("Content-Encoding", "none")
+            drop_request_specific_headers(in_headers)
             url = f'{api_handler.config.api.tsdb_proxy_url}/{request.match_info["tail"]}'
             try:
                 async with api_handler.session.request(
@@ -42,9 +50,7 @@ def tsdb(api_handler: "api.Api") -> Callable[[Request], Awaitable[StreamResponse
                 ) as cr:
                     log.info(f"Proxy tsdb request to: {url} resulted in status={cr.status}")
                     headers = cr.headers.copy()
-                    # since we stream the content (chunked), we are not allowed to set the content length.
-                    headers.popall("Content-Length", "none")
-                    headers.popall("Content-Encoding", "none")
+                    drop_request_specific_headers(headers)
                     via = f"{request.version.major}.{request.version.minor} {hostname()}"
                     headers["Via"] = via
                     headers["ViaResoto"] = via  # the via header might be set by other instances ase well
