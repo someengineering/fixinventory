b37504f5521fe5e2cbc5185206983bfea5c0d341
Matthias Veit <aquamatthias@users.noreply.github.com>
2023-03-30 12:11:40 +0200

    [resotocore][fix] Strip request specific headers and retry (#1524)

--- resotocore/resotocore/web/tsdb.py
+++ resotocore/resotocore/web/tsdb.py
@@ -6,6 +6,7 @@ from typing import Callable, Awaitable
 from aiohttp import ClientConnectionError
 from aiohttp.web import HTTPNotFound, Request, StreamResponse
 from aiohttp.web_exceptions import HTTPBadGateway
+from multidict import CIMultiDict
 
 from resotocore.web import api  # pylint: disable=unused-import # prevent circular import
 from resotocore.web.directives import enable_compression
@@ -23,14 +24,22 @@ def hostname() -> str:
 
 # Proxy request to configured tsdb endpoint
 def tsdb(api_handler: "api.Api") -> Callable[[Request], Awaitable[StreamResponse]]:
+    def drop_request_specific_headers(headers: CIMultiDict[str]) -> None:
+        # since we stream the content (chunked), we are not allowed to set the content length.
+        headers.popall("Content-Length", None)
+        headers.popall("Content-Encoding", None)
+        headers.popall("Connection", None)
+        headers.popall("Transfer-Encoding", None)
+        headers.popall("Accept-Encoding", None)
+        headers.popall("Host", None)
+
     async def proxy_request(request: Request) -> StreamResponse:
         if api_handler.config.api.tsdb_proxy_url:
             in_headers = request.headers.copy()
-            # since we stream the content (chunked), we are not allowed to set the content length.
-            in_headers.popall("Content-Length", "none")
-            in_headers.popall("Content-Encoding", "none")
+            drop_request_specific_headers(in_headers)
             url = f'{api_handler.config.api.tsdb_proxy_url}/{request.match_info["tail"]}'
-            try:
+
+            async def do_request(attempts_left: int) -> StreamResponse:
                 async with api_handler.session.request(
                     request.method,
                     url,
@@ -40,23 +49,44 @@ def tsdb(api_handler: "api.Api") -> Callable[[Request], Awaitable[StreamResponse
                     data=request.content,
                     ssl=api_handler.cert_handler.client_context,
                 ) as cr:
-                    log.info(f"Proxy tsdb request to: {url} resulted in status={cr.status}")
-                    headers = cr.headers.copy()
-                    # since we stream the content (chunked), we are not allowed to set the content length.
-                    headers.popall("Content-Length", "none")
-                    headers.popall("Content-Encoding", "none")
-                    via = f"{request.version.major}.{request.version.minor} {hostname()}"
-                    headers["Via"] = via
-                    headers["ViaResoto"] = via  # the via header might be set by other instances ase well
-                    response = StreamResponse(status=cr.status, reason=cr.reason, headers=headers)
-                    enable_compression(request, response)
-                    await response.prepare(request)
-                    async for data in cr.content.iter_chunked(1024 * 1024):
-                        await response.write(data)
-                    await response.write_eof()
-                    return response
+                    try:
+                        # we see valid requests failing in prometheus with 400, so we also retry client errors
+                        # ideally we would only retry on 5xx errors
+                        if (cr.status == 400 or cr.status >= 500) and attempts_left > 0:
+                            req_header = ", ".join(f"{k}={v}" for k, v in in_headers.items())
+                            req_params = ", ".join(f"{k}={v}" for k, v in request.query.items())
+                            req_body = await request.content.read()
+                            resp_header = ", ".join(f"{k}={v}" for k, v in cr.headers.items())
+                            resp_body = await cr.text()
+                            log.warning(
+                                f"tsdb server returned an error: url:{url}. "
+                                f"Request(headers:{req_header}, params:{req_params}, body:{str(req_body)}) "
+                                f"Response(headers:{resp_header}, status:{cr.status}, body:{resp_body})"
+                            )
+                            cr.close()  # close the connection explicitly, might be pooled otherwise
+                            return await do_request(attempts_left - 1)
+                        else:
+                            log.info(f"Proxy tsdb request to: {url} resulted in status={cr.status}")
+                            headers = cr.headers.copy()
+                            drop_request_specific_headers(headers)
+                            via = f"{request.version.major}.{request.version.minor} {hostname()}"
+                            headers["Via"] = via
+                            headers["ViaResoto"] = via  # the via header might be set by other instances ase well
+                            response = StreamResponse(status=cr.status, reason=cr.reason, headers=headers)
+                            enable_compression(request, response)
+                            await response.prepare(request)
+                            async for data in cr.content.iter_chunked(1024 * 1024):
+                                await response.write(data)
+                            await response.write_eof()
+                            return response
+                    except Exception:
+                        cr.close()  # close connection on any error
+                        raise
+
+            try:
+                return await do_request(attempts_left=3)  # retry the request up to 3 times
             except ClientConnectionError as e:
-                log.warning(f"Proxy tsdb request to: {url} resulted in error={e}")
+                log.warning(f"Proxy tsdb request to: {url} is not reachable {e}")
                 raise HTTPBadGateway(text="tsdb server is not reachable") from e
         else:
             raise HTTPNotFound(text="No tsdb defined. Adjust resoto.core configuration.")
