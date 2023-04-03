a5389ad9f1e3f66dfdb48730bdaa050b26a8925b
Nikita Melkozerov <nikita@some.engineering>
2023-04-03 09:09:05 +0000

    [resotocore][feat] Add the local infrastructure app runtime

---
+++ resotocore/resotocore/infra_apps/local_runtime.py
@@ -0,0 +1,65 @@
+from typing import Optional, Dict, List, Any, AsyncIterator
+from resotocore.infra_apps.runtime import AppResult, Success, Failure
+from resotocore.infra_apps.manifest import AppManifest
+from jinja2 import Environment
+import logging
+from resotocore.types import Json, JsonElement
+from resotocore.validator import Validator
+from resotocore.cli.cli import CLI
+from aiostream import stream
+
+
+log = logging.getLogger(__name__)
+
+
+class LocalResotocoreAppRuntime:
+    """
+    Runtime implementation that runs the Infrastructure App directly on the resotocore.
+    """
+
+    def __init__(self, cli: CLI) -> None:
+        self.cli = cli
+
+    async def generate_template(
+        self, manifest: AppManifest, config: Json, stdin: JsonElement, kwargs: Dict[str, Any]
+    ) -> AsyncIterator[str]:
+        env = Environment(extensions=["jinja2.ext.do", "jinja2.ext.loopcontrols"], enable_async=True)
+        template = env.from_string(manifest.source)
+        # template.globals["search"] = self.cg.search
+
+        async for line in template.generate_async(config=config, **kwargs):
+            log.debug(f"Rendered infrastructure app line: {line}")
+            line = line.strip()
+            if not line:
+                continue
+            yield line
+
+    async def execute(
+        self, manifest: AppManifest, config: Json, stdin: JsonElement, kwargs: Dict[str, Any]
+    ) -> AppResult:
+        """
+        Runtime implementation that runs the app locally.
+        """
+        # todo: validate config
+        # errors = self._validate_config(manifest, config)
+        # if errors:
+        # return Failure(error=f"Invalid config: {errors}")
+
+        try:
+            result = []
+            async for line in self.generate_template(manifest, config, stdin, kwargs):
+                result = await self._interpret_line(line)
+
+            return Success(output=result)
+
+        except Exception as e:
+            log.exception("Error running infrastructure app", e)
+            return Failure(error=f"Error running infrastructure app: {e}")
+
+    async def _interpret_line(self, line: str) -> List[Any]:
+        return await self.cli.execute_cli_command(line, stream.list)
+
+    def _validate_config(self, manifest: AppManifest, config: Json) -> Optional[Json]:
+        v = Validator(schema=manifest.config_schema, allow_unknown=False)
+        result = v.validate(config, normalize=False)
+        return None if result else v.errors
---
+++ resotocore/tests/resotocore/infra_apps/local_runtime_test.py
@@ -0,0 +1,92 @@
+from resotocore.infra_apps.local_runtime import LocalResotocoreAppRuntime
+from resotocore.infra_apps.manifest import AppManifest
+from resotocore.infra_apps.runtime import Success
+import pytest
+from resotocore.cli.model import CLI
+from aiostream import stream
+
+
+@pytest.mark.asyncio
+async def test_template_generation(cli: CLI) -> None:
+    cleanup_untagged = """
+            {%- set tags_part = 'not(has_key(tags, ["' + '", "'.join(config["tags"]) + '"]))' %}
+            {%- set kinds_part = 'is(["' + '", "'.join(config["kinds"]) + '"])' %}
+            {%- set account_parts = [] %}
+            {%- set default_age = config["default"]["age"]|default("2h") %}
+            {%- for cloud_id, account in config["accounts"].items() %}
+                {%- for account_id, account_data in account.items() %}
+                    {%- set age = account_data.get("age", default_age) %}
+                    {%- set account_part = '(/ancestors.cloud.reported.id == "' ~ cloud_id ~ '" and /ancestors.account.reported.id == "' ~ account_id ~ '" and age > ' ~ age ~ ')' %}
+                    {%- do account_parts.append(account_part) %}
+                {%- endfor %}
+            {%- endfor %}
+            {%- set accounts_part = "(" + " or ".join(account_parts) + ")" %}
+            {%- set exclusion_part = "/metadata.protected == false and /metadata.phantom == false and /metadata.cleaned == false" %}
+            {%- set required_tags = ", ".join(config["tags"]) %}
+            {%- set reason = "Missing one or more of required tags " ~ required_tags ~ " and age more than threshold" %}
+            {%- set cleanup_search = 'search ' ~ exclusion_part ~ ' and ' ~ kinds_part ~ ' and ' ~ tags_part ~ ' and ' ~ accounts_part ~ ' | clean "' ~ reason ~ '"' %}
+            {{ cleanup_search }}
+        """
+
+    config = {
+        "tags": ["resoto:cleanup"],
+        "kinds": ["instance"],
+        "default": {"age": "1s"},
+        "accounts": {
+            "digitalocean": {
+                "12345": {"age": "1s"},
+            },
+        },
+    }
+
+    manifest = AppManifest(
+        name="test-app",
+        description="test app description",
+        version="0.0.0",
+        readme="",
+        language="jinja2",
+        url="",
+        icon="",
+        categories=[],
+        default_config=None,
+        config_schema=None,
+        args_schema=[],
+        source=cleanup_untagged,
+    )
+
+    runtime = LocalResotocoreAppRuntime(cli)
+    lines = [line async for line in runtime.generate_template(manifest, config, None, {})]
+    assert lines == [
+        (
+            "search /metadata.protected == false and /metadata.phantom == false and /metadata.cleaned == false "
+            'and is(["instance"]) and not(has_key(tags, ["resoto:cleanup"])) and ((/ancestors.cloud.reported.id'
+            ' == "digitalocean" and /ancestors.account.reported.id == "12345" and age > 1s)) | '
+            'clean "Missing one or more of required tags resoto:cleanup and age more than threshold"'
+        ),
+    ]
+
+
+@pytest.mark.asyncio
+async def test_execute(cli: CLI) -> None:
+    result = await cli.execute_cli_command("echo foo", stream.list)
+
+    source = "echo foo"
+    manifest = AppManifest(
+        name="test-app",
+        description="test app description",
+        version="0.0.0",
+        readme="",
+        language="jinja2",
+        url="",
+        icon="",
+        categories=[],
+        default_config=None,
+        config_schema=None,
+        args_schema=[],
+        source=source,
+    )
+
+    runtime = LocalResotocoreAppRuntime(cli)
+    result = await runtime.execute(manifest, config={}, kwargs={}, stdin={})
+    assert isinstance(result, Success)
+    assert result.output == [["foo"]]
