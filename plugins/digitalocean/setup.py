ecb41509665d39ffac07263e8937b9512b362c6f
Lukas Lösche <lukas@some.engineering>
2023-04-21 19:05:56 +0200

    [resoto][chore] Bump 3.3.4 (#1550)

--- plugins/digitalocean/setup.py
+++ plugins/digitalocean/setup.py
@@ -10,7 +10,7 @@ def read(file_name: str) -> str:
 
 setup(
     name="resoto-plugin-digitalocean",
-    version="3.3.3",
+    version="3.3.4",
     description="Resoto DigitalOcean Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
