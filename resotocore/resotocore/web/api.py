c2bea8a75e1c6364a8aaa3749a8b021cf4673aa2
Lukas Lösche <lukas@some.engineering>
2023-05-12 17:28:14 +0200

    Revert resotocore change

--- resotocore/resotocore/web/api.py
+++ resotocore/resotocore/web/api.py
@@ -135,7 +135,6 @@ AlwaysAllowed = {
     "/static/.*",
     "/system/.*",
     "/ui.*",
-    "/debug/ui/.*",
 }
 # Authorization is not required, but implemented as part of the request handler
 DeferredCheck = {"/events", "/work/queue"}
