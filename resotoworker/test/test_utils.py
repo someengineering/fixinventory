e7223821e354ad4729c0e75772632bc7f2b52bbe
Lukas Lösche <lukas@some.engineering>
2023-05-16 13:36:37 +0200

    [resotoworker][fix] Write home directory files securely (#1585)
    * [resotoworker][fix] Write home directory files securely
    
    * Update tests

--- resotoworker/test/test_utils.py
+++ resotoworker/test/test_utils.py
@@ -1,3 +1,4 @@
+import uuid
 from resotoworker.utils import write_files_to_home_dir, write_utf8_file
 from resotoworker.config import HomeDirectoryFile
 from tempfile import TemporaryDirectory
@@ -10,6 +11,7 @@ def test_write_utf8_file() -> None:
         f = Path(tmpdir) / "foo" / "test.txt"
         write_utf8_file(f, "bar")
         assert f.read_text(encoding="utf-8") == "bar"
+        assert f.stat().st_mode & 0o777 == 0o600
 
 
 class InMemoryFile:
@@ -36,35 +38,35 @@ def test_write_file() -> None:
 
     # relative path
     with InMemoryFile() as f:
-        path = "foo"
+        path = str(uuid.uuid4())
         write_files_to_home_dir(ecfs(path), f.write_to_disk)
-        assert f.file_path == Path.home() / Path("foo")
+        assert f.file_path == Path.home() / path
         assert f.file_content == content
 
     # absolute path outside home directory
     with InMemoryFile() as f:
-        path = "/foo"
+        path = "/" + str(uuid.uuid4())
         write_files_to_home_dir(ecfs(path), f.write_to_disk)
         assert f.file_path is None
         assert f.file_content is None
 
     # path inside home directory
     with InMemoryFile() as f:
-        path = "~/foo"
+        path = "~/" + str(uuid.uuid4())
         write_files_to_home_dir(ecfs(path), f.write_to_disk)
-        assert f.file_path == Path.home() / "foo"
+        assert f.file_path == Path.home() / path.replace("~/", "")
         assert f.file_content == content
 
     # relative path with too many ..
     with InMemoryFile() as f:
-        path = "foo/../../../../../etc/passwd"
+        path = str(uuid.uuid4()) + "/../../../../../etc/passwd"
         write_files_to_home_dir(ecfs(path), f.write_to_disk)
         assert f.file_path is None
         assert f.file_content is None
 
     # absolute path inside home directory
     with InMemoryFile() as f:
-        path = str(Path.home() / "foo")
+        path = str(Path.home() / str(uuid.uuid4()))
         write_files_to_home_dir(ecfs(path), f.write_to_disk)
-        assert f.file_path == Path.home() / "foo"
+        assert f.file_path == Path.home() / path.replace(str(Path.home()) + "/", "")
         assert f.file_content == content
