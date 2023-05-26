7c55677e0f523eba974ad2d60002d3f860034651
Lukas Lösche <lukas@some.engineering>
2023-05-26 18:02:12 +0200

    [resoto][chore] Bump 3.5.0 (#1606)

--- plugins/gcp/setup.py
+++ plugins/gcp/setup.py
@@ -10,7 +10,7 @@ def read(file_name: str) -> str:
 
 setup(
     name="resoto-plugin-gcp",
-    version="3.4.3",
+    version="3.5.0",
     description="Resoto GCP Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
--- plugins/posthog/setup.py
+++ plugins/posthog/setup.py
@@ -10,7 +10,7 @@ def read(file_name: str) -> str:
 
 setup(
     name="resoto-plugin-posthog",
-    version="3.4.3",
+    version="3.5.0",
     description="Resoto Posthog Collector Plugin",
     license="Apache 2.0",
     packages=find_packages(),
