f751e85bd59d5ba5d86cdb7085f39fd73a41f213
Nikita Melkozerov <nikita@some.engineering>
2023-05-26 18:39:32 +0200

    [resotocore][fix] Fix kwargs (#1608)
    * [resotocore][fix] Fix kwargs
    
    * mypy
    
    * mypy fix #2

--- resotocore/resotocore/infra_apps/local_runtime.py
+++ resotocore/resotocore/infra_apps/local_runtime.py
@@ -107,10 +107,7 @@ class LocalResotocoreAppRuntime(Runtime):
                         kwargs[flag] = str_to_type(arg_info[flag])
                     else:
                         kwargs[flag] = arg_info[flag]
-            parser.add_argument(
-                f"--{arg_name}",
-                kwargs=kwargs,
-            )
+            parser.add_argument(f"--{arg_name}", **kwargs)  # type: ignore
 
         return parser.parse_args(argv)
 
