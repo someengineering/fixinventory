58978e25ea28bc7e6822f7ceddc049c0d666f0b8
Nikita Melkozerov <nikita@some.engineering>
2023-05-30 14:44:29 +0000

    fix the tests

--- plugins/k8s/test/collector_test.py
+++ plugins/k8s/test/collector_test.py
@@ -53,8 +53,8 @@ def test_collect() -> None:
     plugin.core_feedback = CoreFeedback("test", "test", "test", Queue())
     # start a collect: use the static file client to get the static json files
     plugin.collect(client_factory=StaticFileClient.static)
-    assert len(plugin.graph.nodes) == 562
-    assert len(plugin.graph.edges) == 850
+    assert len(plugin.graph.nodes) == 565
+    assert len(plugin.graph.edges) == 853
 
 
 def test_tag_update(config_map_in_graph: Tuple[KubernetesConfigMap, Graph, StaticFileClient]) -> None:
