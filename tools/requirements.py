787c67d360fd44b66879868d43743fcbc29d9020
Matthias Veit <aquamatthias@users.noreply.github.com>
2023-07-03 14:00:27 +0200

    [resoto][chore] Fix require and bump dependencies (#1704)

--- tools/requirements.py
+++ tools/requirements.py
@@ -60,13 +60,30 @@ def filter_dependencies(deps: List[str]) -> List[str]:
     return [dep for dep in deps if not any(name in dep for name in filter_out)]
 
 
-def compile_dependencies(name: Optional[str], deps: List[str]) -> None:
+def compile_dependencies(name: Optional[str], deps: List[str], all_dependencies: List[str]) -> List[str]:
+    # If a dependency is defined in all, use that version
+    def dependency_line(dep: str) -> str:
+        d_name, d_version = dep.split("==", maxsplit=1)
+        if defined_in_all := [a for a in all_dependencies if a.startswith(d_name + "==")]:
+            assert len(defined_in_all) == 1, f"More than one dependency with this name: {d_name}"
+            return defined_in_all[0]
+        else:
+            return dep
+
+    print(f"Compile dependencies for {name or 'prod'}")
     delim = "-" + name if name else ""
     with open(f"requirements{delim}-in.txt", "w") as f:
         f.write("\n".join(deps))
     args = "-q --no-annotate --resolver=backtracking --upgrade --allow-unsafe --no-header  --unsafe-package n/a"
     os.system(f"pip-compile {args} --output-file requirements{delim}.txt requirements{delim}-in.txt")
     os.remove(f"requirements{delim}-in.txt")
+    # make sure, none of the filtered dependencies was selected as transitive dependency
+    with open(f"requirements{delim}.txt", "r+") as f:
+        lines = [dependency_line(line) for line in f.readlines() if not any(name in line for name in filter_out)]
+        f.seek(0)
+        f.writelines(lines)
+        f.truncate()
+        return lines
 
 
 def combine_dependencies() -> None:
@@ -76,13 +93,16 @@ def combine_dependencies() -> None:
         prod_dependencies.extend(filter_dependencies(project.dependencies))
         for name, deps in project.optional_dependencies.items():
             optional_dependencies[name].extend(filter_dependencies(deps))
-    all_dependencies = prod_dependencies.copy()
 
-    compile_dependencies(None, prod_dependencies)
+    # gather all dependencies
+    all_dependencies = prod_dependencies.copy()
     for name, deps in optional_dependencies.items():
         all_dependencies.extend(deps)
-        compile_dependencies(name, deps + prod_dependencies)
-    compile_dependencies("all", all_dependencies)
+    all_compiled = compile_dependencies("all", all_dependencies, [])
+
+    compile_dependencies(None, prod_dependencies, all_compiled)
+    for name, deps in optional_dependencies.items():
+        compile_dependencies(name, deps + prod_dependencies, all_compiled)
 
 
 if __name__ == "__main__":
