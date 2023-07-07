f96d442616c0920ab060b43797ac97f79c929a32
Lukas Lösche <lukas@some.engineering>
2023-07-07 17:00:35 +0200

    Combine export_model functions

--- resotolib/resotolib/graph/__init__.py
+++ resotolib/resotolib/graph/__init__.py
@@ -361,24 +361,7 @@ class Graph(networkx.MultiDiGraph):  # type: ignore
                 node.resolve_deferred_connections(self)
 
     def export_model(self, **kwargs: Any) -> List[Json]:
-        """Return the graph node dataclass model in resotocore format"""
-        classes = set()
-        for node in self.nodes:
-            classes.add(type(node))
-        model = resource_classes_to_resotocore_model(classes, aggregate_root=BaseResource, **kwargs)
-
-        # fixme: workaround to report kind
-        for resource_model in model:
-            if resource_model.get("fqn") == "resource":
-                resource_model.get("properties", []).append(
-                    {
-                        "name": "kind",
-                        "kind": "string",
-                        "required": True,
-                        "description": "",
-                    }
-                )
-        return model
+        return export_model(graph=self, **kwargs)
 
     def export_iterator(self) -> GraphExportIterator:
         return GraphExportIterator(self)
@@ -402,9 +385,16 @@ def resource_classes_to_resotocore_model(classes: Set[Type[Any]], **kwargs: Any)
     return list(model.values())
 
 
-def export_model() -> List[Json]:
+def export_model(graph: Optional[Graph] = None, **kwargs: Any) -> List[Json]:
     """Return the graph node dataclass model in resotocore format"""
-    model = dataclasses_to_resotocore_model({BaseResource}, aggregate_root=BaseResource)
+    classes = set()
+    if graph is None:
+        classes.add(BaseResource)
+    else:
+        for node in graph.nodes:
+            classes.add(type(node))
+
+    model = resource_classes_to_resotocore_model(classes, aggregate_root=BaseResource, **kwargs)
     for resource_model in model:
         if resource_model.get("fqn") == "resource":
             resource_model.get("properties", []).append(
