d71bd07a6bb16217fa0e1659d60784c480ce07e4
Lukas Lösche <lukas@some.engineering>
2023-07-25 12:11:59 +0200

    [resoto][chore] Bump 3.6.5 (#1752)

--- resotocore/resotocore/__init__.py
+++ resotocore/resotocore/__init__.py
@@ -11,7 +11,7 @@ __description__ = "resoto core."
 __author__ = "Some Engineering Inc."
 __license__ = "Apache 2.0"
 __copyright__ = "Copyright © 2023 Some Engineering Inc."
-__version__ = "3.6.4"
+__version__ = "3.6.5"
 
 
 def version() -> str:
