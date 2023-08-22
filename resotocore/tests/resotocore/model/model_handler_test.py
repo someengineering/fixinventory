6be6c95d91a3417fc63b7c1a4f46dc6cc08d6f1d
Matthias Veit <matthias_veit@yahoo.de>
2023-08-22 20:23:03 +0200

    remove dummy test

--- resotocore/tests/resotocore/model/model_handler_test.py
+++ resotocore/tests/resotocore/model/model_handler_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from resotocore.db.db_access import DbAccess
 from resotocore.ids import GraphName
-from resotocore.model.model_handler import ModelHandlerDB, code_model
+from resotocore.model.model_handler import ModelHandlerDB
 
 
 @pytest.mark.asyncio
@@ -26,8 +26,3 @@ async def test_uml_generation(db_access: DbAccess) -> None:
         sort_props=False,
     )
     assert b"class graph_root [[#graph_root]] {\n\n}" in image2
-
-
-def test_code_model() -> None:
-    model = code_model()
-    assert len(model.kinds) > 1000
