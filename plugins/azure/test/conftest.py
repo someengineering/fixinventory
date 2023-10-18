1d58f815a337644abd6ceaac63673b3fbc4ad01b
Matthias Veit <matthias_veit@yahoo.de>
2023-10-18 10:50:57 +0200

    ignore attr classes

--- plugins/azure/test/conftest.py
+++ plugins/azure/test/conftest.py
@@ -102,7 +102,7 @@ def builder(
 
 
 def all_props_set(obj: AzureResourceType, ignore_props: Optional[Set[str]] = None) -> None:
-    for field in fields(type(obj)):
+    for field in fields(type(obj)):  # type: ignore
         prop = field.name
         if not prop.startswith("_") and prop not in {
             "account",
