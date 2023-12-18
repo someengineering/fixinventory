e4641e58d89dc46286184c27026cc00637c8db46
Matthias Veit <matthias_veit@yahoo.de>
2023-12-18 11:24:52 +0100

    [resotocore][feat] Add AWS CIS 2.0

--- resotocore/resotocore/db/arangodb_extensions.py
+++ resotocore/resotocore/db/arangodb_extensions.py
@@ -12,23 +12,39 @@ log = logging.getLogger(__name__)
 
 
 class ArangoHTTPClient(HTTPClient):
-    def __init__(self, timeout: int, verify: Union[str, bool, None]):
+    def __init__(
+        self,
+        timeout: int,
+        verify: Union[str, bool, None],
+        retry_attempts: int = 3,
+        backoff_factor: float = 1.0,
+        pool_connections: int = 10,
+        pool_maxsize: int = 20,
+    ):
         log.info(f"Create ArangoHTTPClient with timeout={timeout} and verify={verify}")
-        self.timeout = timeout
-        self.verify = verify
+        self._timeout = timeout
+        self._verify = verify
+        self._retry_attempts = retry_attempts
+        self._backoff_factor = backoff_factor
+        self._pool_connections = pool_connections
+        self._pool_maxsize = pool_maxsize
 
     def create_session(self, host: str) -> Session:
         retry_strategy = Retry(
-            total=3,
-            backoff_factor=1,
+            total=self._retry_attempts,
+            backoff_factor=self._backoff_factor,
             status_forcelist=[429, 500, 502, 503, 504],
             allowed_methods=["HEAD", "GET", "OPTIONS"],
         )
-        http_adapter = HTTPAdapter(max_retries=retry_strategy)
+        http_adapter = HTTPAdapter(
+            pool_connections=self._pool_connections,
+            pool_maxsize=self._pool_maxsize,
+            max_retries=retry_strategy,
+        )
         session = Session()
         session.mount("https://", http_adapter)
         session.mount("http://", http_adapter)
-        session.verify = self.verify
+        session.verify = self._verify
         return session
 
     def send_request(
@@ -41,5 +57,5 @@ class ArangoHTTPClient(HTTPClient):
         data: Union[str, MultipartEncoder, None] = None,
         auth: Optional[Tuple[str, str]] = None,
     ) -> Response:
-        response = session.request(method, url, params, data, headers, auth=auth, timeout=self.timeout)
+        response = session.request(method, url, params, data, headers, auth=auth, timeout=self._timeout)
         return Response(method, response.url, response.headers, response.status_code, response.reason, response.text)
