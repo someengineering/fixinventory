026c7014c7e804b0eaf5cf19b935ffe5f8b6055d
Matthias Veit <matthias_veit@yahoo.de>
2023-12-27 09:39:01 +0100

    fix aws

--- plugins/aws/test/collector_test.py
+++ plugins/aws/test/collector_test.py
@@ -34,7 +34,7 @@ def test_collect(account_collector: AwsAccountCollector) -> None:
     assert len(threading.enumerate()) == 1
     # ensure the correct number of nodes and edges
     assert count_kind(AwsResource) == 214
-    assert len(account_collector.graph.edges) == 499
+    assert len(account_collector.graph.edges) == 503
     assert len(account_collector.graph.deferred_edges) == 2
 
 
