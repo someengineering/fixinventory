18dcc36dec2fce39f6d5a04794fcfe61d1c2bbde
Lukas Lösche <lukas@some.engineering>
2024-04-24 17:40:22 +0200

    Add deregister_image test

--- plugins/aws/test/resources/ec2_test.py
+++ plugins/aws/test/resources/ec2_test.py
@@ -66,6 +66,17 @@ def test_delete_volumes() -> None:
     volume.delete_resource(client, Graph())
 
 
+def test_deregister_image() -> None:
+    image, _ = round_trip_for(AwsEc2Image)
+
+    def validate_delete_args(**kwargs: Any) -> None:
+        assert kwargs["action"] == "deregister-image"
+        assert kwargs["ImageId"] == image.id
+
+    client = cast(AwsClient, SimpleNamespace(call=validate_delete_args))
+    image.delete_resource(client, Graph())
+
+
 def test_snapshots() -> None:
     round_trip_for(AwsEc2Snapshot)
 
