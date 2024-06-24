24baaee820c7c86f915f9ecfd2d80d320f3ec9d7
Matthias Veit <aquamatthias@users.noreply.github.com>
2024-06-24 10:25:00 +0200

    [aws][chore] Ignore props for history (#2121)

--- plugins/aws/fix_plugin_aws/resource/dynamodb.py
+++ plugins/aws/fix_plugin_aws/resource/dynamodb.py
@@ -338,8 +338,8 @@ class AwsDynamoDbPointInTimeRecovery:
         "latest_restorable_date_time": S("LatestRestorableDateTime"),
     }
     status: Optional[str] = field(default=None, metadata={"description": "The current state of point in time recovery:    ENABLED - Point in time recovery is enabled.    DISABLED - Point in time recovery is disabled."})  # fmt: skip
-    earliest_restorable_date_time: Optional[datetime] = field(default=None, metadata={"description": "Specifies the earliest point in time you can restore your table to. You can restore your table to any point in time during the last 35 days."})  # fmt: skip
-    latest_restorable_date_time: Optional[datetime] = field(default=None, metadata={"description": "LatestRestorableDateTime is typically 5 minutes before the current time."})  # fmt: skip
+    earliest_restorable_date_time: Optional[datetime] = field(default=None, metadata={"ignore_history": True, "description": "Specifies the earliest point in time you can restore your table to. You can restore your table to any point in time during the last 35 days."})  # fmt: skip
+    latest_restorable_date_time: Optional[datetime] = field(default=None, metadata={"ignore_history": True, "description": "LatestRestorableDateTime is typically 5 minutes before the current time."})  # fmt: skip
 
 
 @define(eq=False, slots=False)
