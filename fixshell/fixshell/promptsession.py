891d067099ea13a39b746c2a6b4c3a6de893a133
Matthias Veit <matthias_veit@yahoo.de>
2024-07-26 13:55:23 +0200

    better message

--- fixshell/fixshell/promptsession.py
+++ fixshell/fixshell/promptsession.py
@@ -849,7 +849,7 @@ async def core_metadata(
                 cmds.append(evolve(lookup[cmd], name=alias, is_alias=True))
         return cmds, visible_kinds, sorted(known_props)
     except Exception as ex:
-        log.info(f"Can not load metadata from core. No graph? No suggestions as fallback. {ex}")
+        log.info(f"Can not load metadata. Possible Reason: no data collected. No suggestions as fallback.")
         return [], [], []
 
 
