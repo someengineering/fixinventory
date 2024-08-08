96ff81b15b86d8c8a9f62ede3da80642206c0959
Matthias Veit <matthias_veit@yahoo.de>
2024-08-08 09:02:20 +0200

    [core][fix] Move timeline to request time after

--- fixcore/fixcore/db/arango_query.py
+++ fixcore/fixcore/db/arango_query.py
@@ -8,6 +8,7 @@ from typing import Union, List, Tuple, Any, Optional, Dict, Literal, Collection
 
 from arango.typings import Json
 from attrs import evolve
+from math import floor
 
 from fixcore.constants import less_greater_then_operations as lgt_ops, arangodb_matches_null_ops
 from fixcore.db import EstimatedSearchCost, EstimatedQueryCostRating as Rating
@@ -137,18 +138,23 @@ def history_query(db: Any, query_model: QueryModel) -> Tuple[str, Json]:
     return f"""{query_str} FOR result in {cursor}{last_limit} RETURN UNSET(result, {unset_props})""", ctx.bind_vars
 
 
-def history_query_histogram(db: Any, query_model: QueryModel, granularity: timedelta) -> Tuple[str, Json]:
+def history_query_timeline(
+    db: Any, query_model: QueryModel, after: datetime, granularity: timedelta
+) -> Tuple[str, Json]:
     ctx = ArangoQueryContext()
     query = rewrite_query(query_model)
     in_cursor, query_str = query_string(
         db, query, query_model, f"`{db.name}_node_history`", False, ctx, id_column="id", use_fulltext_index=False
     )
     crs = ctx.next_crs()
-    slot = ctx.add_bind_var(granularity.total_seconds() * 1000)
+    gran = granularity.total_seconds()
+    at = after.timestamp()
+    offset = ctx.add_bind_var((at - int(floor(at / gran) * gran)) * 1000)
+    slot = ctx.add_bind_var(gran * 1000)
+    slot_fn = f"DATE_ISO8601((FLOOR(DATE_TIMESTAMP({crs}.changed_at) / @{slot}) * @{slot}) + @{offset})"
     query_str += (
         f" FOR {crs} IN {in_cursor} "
-        f"COLLECT change={crs}.change, at=DATE_ISO8601(FLOOR(DATE_TIMESTAMP({crs}.changed_at) / @{slot}) * @{slot}) "
-        f"WITH COUNT INTO v SORT at ASC "
+        f"COLLECT change={crs}.change, at={slot_fn} WITH COUNT INTO v SORT at ASC "
         'RETURN {"at": at, "group": {"change": change}, "v": v}'
     )
     return query_str, ctx.bind_vars
