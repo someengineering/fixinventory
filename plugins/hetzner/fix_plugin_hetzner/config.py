ee60d66247662d5b6a5249a444c96f1528017f22
Lukas Lösche <lukas@some.engineering>
2024-08-08 17:43:51 +0200

    Update strings

--- plugins/hetzner/fix_plugin_hetzner/config.py
+++ plugins/hetzner/fix_plugin_hetzner/config.py
@@ -5,5 +5,14 @@ from typing import ClassVar, List
 @define
 class HetznerConfig:
     kind: ClassVar[str] = "hetzner"
-    hcloud_project_names: List[str] = field(factory=list, metadata={"description": "Hetzner Cloud project names"})
+    hcloud_project_names: List[str] = field(
+        factory=list,
+        metadata={
+            "description": (
+                "Hetzner Cloud project names - Hetzner has no API to introspect a token, so you need to manually maintain"
+                " the project name associated with an API token. Provide names in the same order as the corresponding API"
+                " tokens."
+            )
+        },
+    )
     hcloud_tokens: List[str] = field(factory=list, metadata={"description": "Hetzner Cloud project API tokens"})
