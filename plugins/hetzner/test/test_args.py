0b9c506a9e195eae8a0ced6cf71f225a6ac8bdbb
Lukas Lösche <lukas@some.engineering>
2024-08-08 17:00:47 +0200

    Update tests

--- plugins/hetzner/test/test_args.py
+++ plugins/hetzner/test/test_args.py
@@ -1,13 +1,8 @@
 from fixlib.args import get_arg_parser
 from fix_plugin_hetzner import HetznerCollectorPlugin
 
-# from fixlib.args import ArgumentParser
-
 
 def test_args():
     arg_parser = get_arg_parser()
     HetznerCollectorPlugin.add_args(arg_parser)
     arg_parser.parse_args()
-
-
-#    assert ArgumentParser.args.example_arg is None
--- plugins/hetzner/test/test_config.py
+++ plugins/hetzner/test/test_config.py
@@ -1,11 +1,9 @@
 from fixlib.config import Config
-from fix_plugin_example_collector import ExampleCollectorPlugin
+from fix_plugin_hetzner import HetznerCollectorPlugin
 
 
 def test_config():
     config = Config("dummy", "dummy")
-    ExampleCollectorPlugin.add_config(config)
+    HetznerCollectorPlugin.add_config(config)
     Config.init_default_config()
-
-
-#    assert Config.example.region is None
+    assert Config.hetzner.hcloud_tokens == []
