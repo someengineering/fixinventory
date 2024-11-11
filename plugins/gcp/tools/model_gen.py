2ebe747aab2a0bc42a1a0067a481f6998043f3ca
Kirill <k.melkozerov@gmail.com>
2024-11-11 14:37:56 +0000

    rebase main

--- plugins/gcp/tools/model_gen.py
+++ plugins/gcp/tools/model_gen.py
@@ -521,9 +521,9 @@ apis = [
     # ("container", "v1", "Container", ["UsableSubnetwork"]),
     # ("sqladmin", "v1", "Sql", ["Tier"]),
     # ("cloudbilling", "v1", "", []),
-    # ("storage", "v1", "", [])
-    # # ("aiplatform", "v1", "", [])
-    ("firestore", "v1", "", []),
+    # ("storage", "v1", "", []),
+    # # ("aiplatform", "v1", "", []),
+    # ("firestore", "v1", "", []),
     ("file", "v1", "", [])
 ]
 
