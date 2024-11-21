f2f3c927353c1d03cebca42406453988b1dadba6
Kirill <k.melkozerov@gmail.com>
2024-11-21 10:20:29 +0000

    fix syntax test

--- plugins/gcp/fix_plugin_gcp/resources/aiplatform.py
+++ plugins/gcp/fix_plugin_gcp/resources/aiplatform.py
@@ -50,7 +50,6 @@ class AIPlatformRegionFilter:
     def collect_resources(cls, builder: GraphBuilder, **kwargs: Any) -> List[GcpResource]:
         # Default behavior: in case the class has an ApiSpec, call the api and call collect.
         if issubclass(cls, GcpResource):
-            region_name = "global" if not builder.region else builder.region.safe_name
             if spec := cls.api_spec:
                 expected_errors = GcpExpectedErrorCodes | (spec.expected_errors or set()) | {"HttpError:none:none"}
                 with GcpErrorHandler(
