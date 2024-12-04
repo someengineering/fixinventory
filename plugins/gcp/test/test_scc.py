505ddb2272937ba678f1423f2b34e8aeb9a6b37a
Kirill <k.melkozerov@gmail.com>
2024-12-04 17:12:50 +0000

    fix mypy and tests

--- plugins/gcp/test/test_scc.py
+++ plugins/gcp/test/test_scc.py
@@ -7,13 +7,12 @@ from fix_plugin_gcp.resources.scc import GcpSccFinding
 from .random_client import roundtrip
 
 
-class DefaultDict(dict):
-    # for random location name we use the default global location
-    def __init__(self, default_value: Any, *args, **kwargs):
+class DefaultDict(dict):  # type: ignore
+    def __init__(self, default_value: Any, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.default_value = default_value
 
-    def get(self, key, default=None):
+    def get(self, key: str, default: Any = None):
         if key in self:
             return super().get(key, default)
         return self.default_value
@@ -21,6 +20,7 @@ class DefaultDict(dict):
 
 def test_gcp_scc_findings(random_builder: GraphBuilder) -> None:
     firewall = roundtrip(GcpFirewall, random_builder)
+    # for random location name we will use the default global location
     random_builder.region_by_name = DefaultDict(random_builder.fallback_global_region)
     GcpSccFinding.collect_resources(random_builder)
 
